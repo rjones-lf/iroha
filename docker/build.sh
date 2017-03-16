77c0a38c83c6bf73b6ab78e85c140c813d136a75
Bogdan Vaneev <warchantua@gmail.com>
2017-03-16 11:19:06 +0300

    New build system [wip]

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# for error handling:
+set -o pipefail  # trace ERR through pipes
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 
 if [ -z ${IROHA_HOME} ]; then
     echo "[FATAL] Empty variable IROHA_HOME"
@@ -10,33 +15,37 @@ error(){
 	exit 1
 }
 
-# build iroha-dev image
-if ! docker images hyperledger/iroha-dev | grep -q hyperledger/iroha-dev; then
-  docker build -t hyperledger/iroha-dev ${IROHA_HOME}/docker/dev 
-  if [ ! $? ]; then 
-      error "can not build iroha-dev; exit code: $?"
-  fi
-fi
+SOURCE=${IROHA_HOME}
+BUILD=/tmp/iroha-build
+TINY=${IROHA_HOME}/docker/tiny
+RELEASE=${TINY}/iroha
 
-# run dev container to build iroha
-docker run -i \
-    -v ${IROHA_HOME}/docker/build:/build \
-    -v ${IROHA_HOME}:/opt/iroha \
-    hyperledger/iroha-dev \
-    sh << COMMANDS
-    # everything between COMMANDS will be executed inside a container
-    cd /opt/iroha/docker/dev/scripts
-    ./build-iroha.sh Release || (echo "[-] Can't build iroha" && exit 1)
-    ./mktar-iroha.sh || (echo "[-] Can't make tarball" && exit 1)
-    # at this step we have /tmp/iroha.tar 
-    (cp /tmp/iroha.tar /build/iroha.tar || (echo "[-] FAILED!" && exit 1))
-COMMANDS
-if [ ! $? ]; then 
-    error "can not build iroha; exit code: $?"
-fi
+mkdir -p ${BUILD}
+cd ${BUILD}
+cmake ${IROHA_HOME} -DCMAKE_BUILD_TYPE=Release
+make -j 10
 
-# build hyperledger/iroha-docker container
-docker build -t hyperledger/iroha-docker ${IROHA_HOME}/docker/build
-if [ ! $? ]; then 
-    error "can not build iroha; exit code: $?"
-fi
+# copy libs used by iroha
+LIBS=$(ldd $BUILD/bin/iroha-main | cut -f 2 | cut -d " " -f 3)
+mkdir -p $RELEASE/lib
+cp -H $LIBS $RELEASE/lib
+
+rsync -avr ${BUILD}/bin ${RELEASE} && \
+rsync -avr ${IROHA_HOME}/config ${RELEASE} || error "Can not copy release files"
+
+
+docker build -t hyperledger/iroha-docker ${TINY}
+
+# clean up
+rm -rf ${TINY}/iroha
+
+
+cat << "EOF"
+ _______   ______   .__   __.  _______ 
+|       \ /  __  \  |  \ |  | |   ____|
+|  .--.  |  |  |  | |   \|  | |  |__   
+|  |  |  |  |  |  | |  . `  | |   __|  
+|  '--'  |  `--'  | |  |\   | |  |____ 
+|_______/ \______/  |__| \__| |_______|
+                                      
+EOF
