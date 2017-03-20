9028092d2eb75b028ccaf1ce96b62fcdcc8a9738
mizuki.sonoko <mizuki.sonoko@gmail.com>
2017-03-20 16:09:13 +0900

    Add scenario test

--- test/module/service/executor_test.cpp
+++ test/module/service/executor_test.cpp
@@ -301,7 +301,7 @@ TEST(ScenarioTest, TaxTransfer) {
 
     Api::Transaction tx;
     Api::BaseObject value;
-    value.set_valueint(100);
+    value.set_valueint(50);
 
     Api::BaseObject author;
     author.set_valuestring(publicKey3);
@@ -327,12 +327,447 @@ TEST(ScenarioTest, TaxTransfer) {
     executor::execute(tx);
 
     {
+        Api::Asset asset1 = repository::asset::find(publicKey3, assetName1);
+        ASSERT_STREQ(asset1.name().c_str(), assetName1);
+        ASSERT_TRUE(asset1.value().at("value").valueint() == 5);
+    }
+    {
         Api::Asset asset1 = repository::asset::find(publicKey2, assetName1);
         ASSERT_STREQ(asset1.name().c_str(), assetName1);
-        std::cout << asset1.value().at("value").valueint() << std::endl;
-        ASSERT_TRUE(asset1.value().at("value").valueint() == 90);
+        ASSERT_TRUE(asset1.value().at("value").valueint() == 145);
+    }
+    {
+        Api::Asset asset1 = repository::asset::find(publicKey1, assetName1);
+        ASSERT_STREQ(asset1.name().c_str(), assetName1);
+        ASSERT_TRUE(asset1.value().at("value").valueint() == 50);
     }
     removeData(publicKey1);
     removeData(publicKey2);
     removeData(publicKey3);
+}
+
+TEST(ScenarioTest, CurrencyTransfer) {
+
+    const auto name1 = "mizuki";
+    const auto name2 = "iori";
+    const auto name3 = "maruuchi";
+
+    const auto assetName1 = "naocoin";
+    const auto assetName2 = "kayanocoin";
+
+    //const auto privateKey = "iOHEkcoNgABDt9lz+ndjlLl5dSBkutleGerCSOGoP0nF3vArKBO7oyIxvoDSH+RGkcHRgQYgpRhRq3WSy8MYYA==";
+    const auto publicKey1 = "mXVNEpTcUiMUDZlAlMhmubbJXNqvwNlpSVXcFdu3qt4=";
+    const auto publicKey2 = "p8v1bQ76tHmD3fZrYDWBvzn+aJspfaBw8OwNg+Cx8AI=";
+
+    {
+        const auto type = "add";
+        Api::Transaction tx;
+        tx.set_senderpubkey(publicKey1);
+        tx.set_type(type);
+        tx.mutable_account()->CopyFrom(makeAccount(
+                publicKey1, name1, {assetName1}
+        ));
+        executor::execute(tx);
+        Api::Account account = repository::account::find(publicKey1);
+        ASSERT_STREQ(account.publickey().c_str(), publicKey1);
+        ASSERT_STREQ(account.name().c_str(), name1);
+        IROHA_ASSERT_TRUE(account.assets_size() == 1);
+        ASSERT_STREQ(account.assets(0).c_str(), assetName1);
+
+        Api::BaseObject bobj;
+        bobj.set_valueint(100);
+        std::unordered_map<std::string,Api::BaseObject> prop;
+        prop["value"] = bobj;
+
+        tx.Clear();
+        tx.set_senderpubkey(publicKey1);
+        tx.set_type(type);
+        tx.mutable_asset()->CopyFrom(makeAsset(
+                assetName1, prop
+        ));
+        executor::execute(tx);
+    }
+    {
+        const auto type = "add";
+        Api::Transaction tx;
+        tx.set_senderpubkey(publicKey2);
+        tx.set_type(type);
+        tx.mutable_account()->CopyFrom(makeAccount(
+                publicKey2, name2, {assetName1}
+        ));
+        executor::execute(tx);
+        Api::Account account = repository::account::find(publicKey2);
+        ASSERT_STREQ(account.publickey().c_str(), publicKey2);
+        ASSERT_STREQ(account.name().c_str(), name2);
+        IROHA_ASSERT_TRUE(account.assets_size() == 1);
+        ASSERT_STREQ(account.assets(0).c_str(), assetName1);
+
+        Api::BaseObject bobj;
+        bobj.set_valueint(100);
+        std::unordered_map<std::string,Api::BaseObject> prop;
+        prop["value"] = bobj;
+
+        tx.Clear();
+        tx.set_senderpubkey(publicKey2);
+        tx.set_type(type);
+        tx.mutable_asset()->CopyFrom(makeAsset(
+                assetName1, prop
+        ));
+        executor::execute(tx);
+    }
+
+    const auto type = "transfer";
+
+    Api::Transaction tx;
+    Api::BaseObject value;
+    value.set_valueint(50);
+
+    std::unordered_map<std::string,Api::BaseObject> prop;
+    prop["value"] = value;
+
+    tx.set_senderpubkey(publicKey1);
+    tx.set_receivepubkey(publicKey2);
+    tx.set_type(type);
+    tx.mutable_asset()->CopyFrom(makeAsset(
+        assetName1, prop
+    ));
+    executor::execute(tx);
+
+    {
+        Api::Asset asset1 = repository::asset::find(publicKey2, assetName1);
+        ASSERT_STREQ(asset1.name().c_str(), assetName1);
+        ASSERT_TRUE(asset1.value().at("value").valueint() == 150);
+    }
+    {
+        Api::Asset asset1 = repository::asset::find(publicKey1, assetName1);
+        ASSERT_STREQ(asset1.name().c_str(), assetName1);
+        ASSERT_TRUE(asset1.value().at("value").valueint() == 50);
+    }
+    removeData(publicKey1);
+    removeData(publicKey2);
+}
+
+
+TEST(ScenarioTest, CurrencyTransferFailedNotEnoughValue) {
+
+    const auto name1 = "mizuki";
+    const auto name2 = "iori";
+    const auto name3 = "maruuchi";
+
+    const auto assetName1 = "naocoin";
+    const auto assetName2 = "kayanocoin";
+
+    //const auto privateKey = "iOHEkcoNgABDt9lz+ndjlLl5dSBkutleGerCSOGoP0nF3vArKBO7oyIxvoDSH+RGkcHRgQYgpRhRq3WSy8MYYA==";
+    const auto publicKey1 = "mXVNEpTcUiMUDZlAlMhmubbJXNqvwNlpSVXcFdu3qt4=";
+    const auto publicKey2 = "p8v1bQ76tHmD3fZrYDWBvzn+aJspfaBw8OwNg+Cx8AI=";
+
+    {
+        const auto type = "add";
+        Api::Transaction tx;
+        tx.set_senderpubkey(publicKey1);
+        tx.set_type(type);
+        tx.mutable_account()->CopyFrom(makeAccount(
+                publicKey1, name1, {assetName1}
+        ));
+        executor::execute(tx);
+        Api::Account account = repository::account::find(publicKey1);
+        ASSERT_STREQ(account.publickey().c_str(), publicKey1);
+        ASSERT_STREQ(account.name().c_str(), name1);
+        IROHA_ASSERT_TRUE(account.assets_size() == 1);
+        ASSERT_STREQ(account.assets(0).c_str(), assetName1);
+
+        Api::BaseObject bobj;
+        bobj.set_valueint(10);
+        std::unordered_map<std::string,Api::BaseObject> prop;
+        prop["value"] = bobj;
+
+        tx.Clear();
+        tx.set_senderpubkey(publicKey1);
+        tx.set_type(type);
+        tx.mutable_asset()->CopyFrom(makeAsset(
+                assetName1, prop
+        ));
+        executor::execute(tx);
+    }
+    {
+        const auto type = "add";
+        Api::Transaction tx;
+        tx.set_senderpubkey(publicKey2);
+        tx.set_type(type);
+        tx.mutable_account()->CopyFrom(makeAccount(
+                publicKey2, name2, {assetName1}
+        ));
+        executor::execute(tx);
+        Api::Account account = repository::account::find(publicKey2);
+        ASSERT_STREQ(account.publickey().c_str(), publicKey2);
+        ASSERT_STREQ(account.name().c_str(), name2);
+        IROHA_ASSERT_TRUE(account.assets_size() == 1);
+        ASSERT_STREQ(account.assets(0).c_str(), assetName1);
+
+        Api::BaseObject bobj;
+        bobj.set_valueint(100);
+        std::unordered_map<std::string,Api::BaseObject> prop;
+        prop["value"] = bobj;
+
+        tx.Clear();
+        tx.set_senderpubkey(publicKey2);
+        tx.set_type(type);
+        tx.mutable_asset()->CopyFrom(makeAsset(
+                assetName1, prop
+        ));
+        executor::execute(tx);
+    }
+
+    const auto type = "transfer";
+
+    Api::Transaction tx;
+    Api::BaseObject value;
+    value.set_valueint(50);
+
+    std::unordered_map<std::string,Api::BaseObject> prop;
+    prop["value"] = value;
+
+    tx.set_senderpubkey(publicKey1);
+    tx.set_receivepubkey(publicKey2);
+    tx.set_type(type);
+    tx.mutable_asset()->CopyFrom(makeAsset(
+            assetName1, prop
+    ));
+    executor::execute(tx);
+
+    {
+    Api::Asset asset1 = repository::asset::find(publicKey2, assetName1);
+    ASSERT_STREQ(asset1.name().c_str(), assetName1);
+    ASSERT_TRUE(asset1.value().at("value").valueint() == 100);
+    }
+    {
+    Api::Asset asset1 = repository::asset::find(publicKey1, assetName1);
+    ASSERT_STREQ(asset1.name().c_str(), assetName1);
+    ASSERT_TRUE(asset1.value().at("value").valueint() == 10);
+    }
+    removeData(publicKey1);
+    removeData(publicKey2);
+}
+
+TEST(ScenarioTest, CurrencyTransferEqualValue) {
+
+    const auto name1 = "mizuki";
+    const auto name2 = "iori";
+    const auto name3 = "maruuchi";
+
+    const auto assetName1 = "naocoin";
+    const auto assetName2 = "kayanocoin";
+
+    //const auto privateKey = "iOHEkcoNgABDt9lz+ndjlLl5dSBkutleGerCSOGoP0nF3vArKBO7oyIxvoDSH+RGkcHRgQYgpRhRq3WSy8MYYA==";
+    const auto publicKey1 = "mXVNEpTcUiMUDZlAlMhmubbJXNqvwNlpSVXcFdu3qt4=";
+    const auto publicKey2 = "p8v1bQ76tHmD3fZrYDWBvzn+aJspfaBw8OwNg+Cx8AI=";
+
+    {
+        const auto type = "add";
+        Api::Transaction tx;
+        tx.set_senderpubkey(publicKey1);
+        tx.set_type(type);
+        tx.mutable_account()->CopyFrom(makeAccount(
+                publicKey1, name1, {assetName1}
+        ));
+        executor::execute(tx);
+        Api::Account account = repository::account::find(publicKey1);
+        ASSERT_STREQ(account.publickey().c_str(), publicKey1);
+        ASSERT_STREQ(account.name().c_str(), name1);
+        IROHA_ASSERT_TRUE(account.assets_size() == 1);
+        ASSERT_STREQ(account.assets(0).c_str(), assetName1);
+
+        Api::BaseObject bobj;
+        bobj.set_valueint(10);
+        std::unordered_map<std::string,Api::BaseObject> prop;
+        prop["value"] = bobj;
+
+        tx.Clear();
+        tx.set_senderpubkey(publicKey1);
+        tx.set_type(type);
+        tx.mutable_asset()->CopyFrom(makeAsset(
+                assetName1, prop
+        ));
+        executor::execute(tx);
+    }
+    {
+        const auto type = "add";
+        Api::Transaction tx;
+        tx.set_senderpubkey(publicKey2);
+        tx.set_type(type);
+        tx.mutable_account()->CopyFrom(makeAccount(
+                publicKey2, name2, {assetName1}
+        ));
+        executor::execute(tx);
+        Api::Account account = repository::account::find(publicKey2);
+        ASSERT_STREQ(account.publickey().c_str(), publicKey2);
+        ASSERT_STREQ(account.name().c_str(), name2);
+        IROHA_ASSERT_TRUE(account.assets_size() == 1);
+        ASSERT_STREQ(account.assets(0).c_str(), assetName1);
+
+        Api::BaseObject bobj;
+        bobj.set_valueint(100);
+        std::unordered_map<std::string,Api::BaseObject> prop;
+        prop["value"] = bobj;
+
+        tx.Clear();
+        tx.set_senderpubkey(publicKey2);
+        tx.set_type(type);
+        tx.mutable_asset()->CopyFrom(makeAsset(
+                assetName1, prop
+        ));
+        executor::execute(tx);
+    }
+
+    const auto type = "transfer";
+
+    Api::Transaction tx;
+    Api::BaseObject value;
+    value.set_valueint(10);
+
+    std::unordered_map<std::string,Api::BaseObject> prop;
+    prop["value"] = value;
+
+    tx.set_senderpubkey(publicKey1);
+    tx.set_receivepubkey(publicKey2);
+    tx.set_type(type);
+    tx.mutable_asset()->CopyFrom(makeAsset(
+            assetName1, prop
+    ));
+    executor::execute(tx);
+
+    {
+        Api::Asset asset1 = repository::asset::find(publicKey2, assetName1);
+        ASSERT_STREQ(asset1.name().c_str(), assetName1);
+        std::cout << asset1.value().at("value").valueint() ;
+        ASSERT_TRUE(asset1.value().at("value").valueint() == 110);
+    }
+    {
+        Api::Asset asset1 = repository::asset::find(publicKey1, assetName1);
+        ASSERT_STREQ(asset1.name().c_str(), assetName1);
+        ASSERT_TRUE(asset1.value().at("value").valueint() == 0);
+    }
+    removeData(publicKey1);
+    removeData(publicKey2);
+}
+
+TEST(ScenarioTest, MultiChatMessanger) {
+
+    const auto name1 = "mizuki";
+    const auto name2 = "iori";
+    const auto name3 = "maruuchi";
+
+    const auto assetName1 = "naocoin";
+    const auto assetName2 = "kayanocoin";
+
+    //const auto privateKey = "iOHEkcoNgABDt9lz+ndjlLl5dSBkutleGerCSOGoP0nF3vArKBO7oyIxvoDSH+RGkcHRgQYgpRhRq3WSy8MYYA==";
+    const auto publicKey1 = "mXVNEpTcUiMUDZlAlMhmubbJXNqvwNlpSVXcFdu3qt4=";
+    const auto publicKey2 = "p8v1bQ76tHmD3fZrYDWBvzn+aJspfaBw8OwNg+Cx8AI=";
+
+    {
+        const auto type = "add";
+        Api::Transaction tx;
+        tx.set_senderpubkey(publicKey1);
+        tx.set_type(type);
+        tx.mutable_account()->CopyFrom(makeAccount(
+                publicKey1, name1, {assetName1}
+        ));
+        executor::execute(tx);
+        Api::Account account = repository::account::find(publicKey1);
+        ASSERT_STREQ(account.publickey().c_str(), publicKey1);
+        ASSERT_STREQ(account.name().c_str(), name1);
+        IROHA_ASSERT_TRUE(account.assets_size() == 1);
+        ASSERT_STREQ(account.assets(0).c_str(), assetName1);
+
+        std::unordered_map<std::string,Api::BaseObject> prop;
+
+        Api::BaseObject nanaobj;
+        nanaobj.set_valueint(5);
+        prop["nana"] = nanaobj;
+
+        Api::BaseObject suzuobj;
+        nanaobj.set_valueint(3);
+        prop["suzuobj"] = nanaobj;
+
+        tx.Clear();
+        tx.set_senderpubkey(publicKey1);
+        tx.set_type(type);
+        tx.mutable_asset()->CopyFrom(makeAsset(
+                assetName1, prop
+        ));
+        executor::execute(tx);
+    }
+    {
+        const auto type = "add";
+        Api::Transaction tx;
+        tx.set_senderpubkey(publicKey2);
+        tx.set_type(type);
+        tx.mutable_account()->CopyFrom(makeAccount(
+                publicKey2, name2, {assetName1}
+        ));
+        executor::execute(tx);
+        Api::Account account = repository::account::find(publicKey2);
+        ASSERT_STREQ(account.publickey().c_str(), publicKey2);
+        ASSERT_STREQ(account.name().c_str(), name2);
+        IROHA_ASSERT_TRUE(account.assets_size() == 1);
+        ASSERT_STREQ(account.assets(0).c_str(), assetName1);
+
+        std::unordered_map<std::string,Api::BaseObject> prop;
+
+        Api::BaseObject nanaobj;
+        nanaobj.set_valueint(1);
+        prop["nana"] = nanaobj;
+
+        Api::BaseObject suzuobj;
+        nanaobj.set_valueint(2);
+        prop["suzuobj"] = nanaobj;
+
+        tx.Clear();
+        tx.set_senderpubkey(publicKey2);
+        tx.set_type(type);
+        tx.mutable_asset()->CopyFrom(makeAsset(
+                assetName1, prop
+        ));
+        executor::execute(tx);
+    }
+
+    const auto type = "transfer";
+
+    Api::Transaction tx;
+    Api::BaseObject value;
+    value.set_valueint(1);
+
+    Api::BaseObject target;
+    target.set_valuestring("nana");
+
+    Api::BaseObject assetType;
+    assetType.set_valuestring("multi_message");
+
+    std::unordered_map<std::string,Api::BaseObject> prop;
+    prop["targetName"] = target;
+    prop["value"] = value;
+    prop["type"] = assetType;
+
+    tx.set_senderpubkey(publicKey1);
+    tx.set_receivepubkey(publicKey2);
+    tx.set_type(type);
+    tx.mutable_asset()->CopyFrom(makeAsset(
+        assetName1, prop
+    ));
+    executor::execute(tx);
+
+    {
+        Api::Asset asset1 = repository::asset::find(publicKey2, assetName1);
+        ASSERT_STREQ(asset1.name().c_str(), assetName1);
+        std::cout << asset1.value().at("nana").valueint();
+        ASSERT_TRUE(asset1.value().at("nana").valueint() == 2);
+    }
+    {
+        Api::Asset asset1 = repository::asset::find(publicKey1, assetName1);
+        ASSERT_STREQ(asset1.name().c_str(), assetName1);
+        std::cout << asset1.value().at("nana").valueint();
+        ASSERT_TRUE(asset1.value().at("nana").valueint() == 4);
+    }
+    removeData(publicKey1);
+    removeData(publicKey2);
 }
