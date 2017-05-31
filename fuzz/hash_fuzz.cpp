a53ac9730d9fc2b7e50041111f543e32d466fb37
Kitsu <mail@kitsu.me>
2017-05-31 16:59:43 +0300

    Base hash fuzzer

---
+++ fuzz/hash_fuzz.cpp
@@ -0,0 +1,11 @@
+#include "stdint.h"
+#include "stddef.h"
+
+#include <string>
+#include <crypto/hash.hpp>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  std::string s((const char*)data, size);
+  hash::sha3_256_hex(s);
+  return 0;
+}
