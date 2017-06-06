c4454fe570449f9af337ab8501c0c8df4cd21ed3
Kitsu <mail@kitsu.me>
2017-06-06 12:41:43 +0300

    Add few more fuzzers for hash and b64-enc

--- fuzz/core/crypto/hash256_fuzz.cpp
+++ fuzz/core/crypto/hash256_fuzz.cpp
@@ -2,10 +2,11 @@
 #include "stddef.h"
 
 #include <string>
+#include <vector>
 #include <crypto/hash.hpp>
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   std::string s((const char*)data, size);
-  hash::sha3_256_hex(s);
+  hash::sha3_512_hex(s);
   return 0;
 }
