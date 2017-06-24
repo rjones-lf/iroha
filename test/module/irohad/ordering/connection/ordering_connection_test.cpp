3f35e73423dee674daf6daebba1f366afd8cca8c
motxx <motohiko.ave@gmail.com>
2017-06-25 00:38:17 +0900

    Add ordering_connection_test.cpp

---
+++ test/module/irohad/ordering/connection/ordering_connection_test.cpp
@@ -0,0 +1,77 @@
+/*
+Copyright Soramitsu Co., Ltd. 2016 All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <gtest/gtest.h>
+#include <ordering/connection/client.hpp>
+#include <ordering/connection/service.hpp>
+#include <ordering/observer.hpp>
+#include <main/server_runner.hpp>
+#include <thread>
+#include <endpoint.grpc.pb.h>
+
+using iroha::protocol::Transaction;
+
+class OrderingConnectionTest : public ::testing::Test {
+protected:
+  virtual void SetUp() {
+    serverRunner_.reset(new ServerRunner("0.0.0.0", 50051, {
+      &service_
+    }));
+    running_ = false;
+  }
+
+  virtual void TearDown() {
+    if (running_) {
+      serverRunner_->shutdown();
+      serverThread_.join();
+    }
+  }
+
+  void RunServer() {
+    serverThread_ = std::thread(&IServerRunner::run, std::ref(*serverRunner_));
+    serverRunner_->waitForServersReady();
+    running_ = true;
+  }
+
+private:
+  bool running_;
+  api::OrderingService service_;
+  std::unique_ptr<IServerRunner> serverRunner_;
+  std::thread serverThread_;
+};
+
+/**
+ * fails connection because server isn't running.
+ */
+TEST_F(CommandConnectionTest, FailConnectionWhenNotRunningServer) {
+  Transaction tx;
+  auto response = ordering::connection::sendTransaction(tx, "0.0.0.0");
+  ASSERT_EQ(response.code(), iroha::protocol::ResponseCode::FAIL);
+  ASSERT_STREQ(response.message().c_str(), "connection failed. cannot send transaction.");
+}
+
+/**
+ * success connection, but fails stateful validation.
+ */
+TEST_F(CommandConnectionTest, SuccessConnectionWhenRunningServer) {
+  RunServer();
+
+  ordering::observer::initialize();
+  Transaction tx;
+  auto response = ordering::connection::sendTransaction(tx, "0.0.0.0");
+  ASSERT_EQ(response.code(), iroha::protocol::ResponseCode::FAIL);
+  ASSERT_STREQ(response.message().c_str(), "failed stateless validation.");
+}
