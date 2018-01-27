cefd8f39e94173b3a4b691f1c838a1021180133d
Artyom Bakhtin <a@bakhtin.net>
2018-01-27 16:37:51 +0300

    Jenkinsfile: macos ccache fix, macos relative path binaries, post step timeout, coverage as a cmake target
    Signed-off-by: Artyom Bakhtin <a@bakhtin.net>

--- .jenkinsci/debug-build.groovy
+++ .jenkinsci/debug-build.groovy
@@ -19,13 +19,13 @@ def doDebugBuild(coverageEnabled=false) {
   // speeds up consequent image builds as we simply tag them 
   sh "docker pull ${DOCKER_BASE_IMAGE_DEVELOP}"
   if (env.BRANCH_NAME == 'develop') {
-    iC = docker.build("hyperledger/iroha:${GIT_COMMIT}-${BUILD_NUMBER}", "--build-arg PARALLELISM=${parallelism} -f /tmp/${env.GIT_COMMIT}/Dockerfile /tmp/${env.GIT_COMMIT}")
+    iC = docker.build("hyperledger/iroha:${GIT_COMMIT}-${BUILD_NUMBER}", "--build-arg PARALLELISM=${env.parallelism} -f /tmp/${env.GIT_COMMIT}/Dockerfile /tmp/${env.GIT_COMMIT}")
     docker.withRegistry('https://registry.hub.docker.com', 'docker-hub-credentials') {
       iC.push("${platform}-develop")
     }
   }
   else {
-    iC = docker.build("hyperledger/iroha-workflow:${GIT_COMMIT}-${BUILD_NUMBER}", "-f /tmp/${env.GIT_COMMIT}/Dockerfile /tmp/${env.GIT_COMMIT} --build-arg PARALLELISM=${parallelism}")
+    iC = docker.build("hyperledger/iroha-workflow:${GIT_COMMIT}-${BUILD_NUMBER}", "-f /tmp/${env.GIT_COMMIT}/Dockerfile /tmp/${env.GIT_COMMIT} --build-arg PARALLELISM=${env.parallelism}")
   }
   iC.inside(""
     + " -e IROHA_POSTGRES_HOST=${env.IROHA_POSTGRES_HOST}"
@@ -36,6 +36,10 @@ def doDebugBuild(coverageEnabled=false) {
     + " -v /var/jenkins/ccache:${CCACHE_DIR}") {
 
     def scmVars = checkout scm
+    def cmakeOptions = ""
+    if ( coverageEnabled ) {
+      cmakeOptions = " -DCOVERAGE=ON "
+    }
     env.IROHA_VERSION = "0x${scmVars.GIT_COMMIT}"
     env.IROHA_HOME = "/opt/iroha"
     env.IROHA_BUILD = "${env.IROHA_HOME}/build"
@@ -48,16 +52,22 @@ def doDebugBuild(coverageEnabled=false) {
     """  
     sh """
       cmake \
-        -DCOVERAGE=ON \
         -DTESTING=ON \
         -H. \
         -Bbuild \
         -DCMAKE_BUILD_TYPE=Debug \
-        -DIROHA_VERSION=${env.IROHA_VERSION}
+        -DIROHA_VERSION=${env.IROHA_VERSION} \
+        ${cmakeOptions}
     """
-    sh "cmake --build build -- -j${parallelism}"
+    sh "cmake --build build -- -j${env.parallelism}"
     sh "ccache --show-stats"
-    sh "cmake --build build --target test"
+    if ( coverageEnabled ) {
+      sh "cmake --build build --target coverage.init.info"
+    }
+    def testExitCode = sh(script: 'cmake --build build --target test', returnStatus: true)
+    if (testExitCode != 0) {
+      currentBuild.result = "UNSTABLE"
+    }
     sh "cmake --build build --target cppcheck"
 
     if ( coverageEnabled ) {
@@ -75,9 +85,8 @@ def doDebugBuild(coverageEnabled=false) {
         """
       }
 
-      sh "lcov --capture --directory build --config-file .lcovrc --output-file build/reports/coverage_full.info"
-      sh "lcov --remove build/reports/coverage_full.info '/usr/*' 'schema/*' --config-file .lcovrc -o build/reports/coverage_full_filtered.info"
-      sh "python /tmp/lcov_cobertura.py build/reports/coverage_full_filtered.info -o build/reports/coverage.xml"                
+      sh "cmake --build build --target coverage.info"
+      sh "python /tmp/lcov_cobertura.py build/reports/coverage.info -o build/reports/coverage.xml"
       cobertura autoUpdateHealth: false, autoUpdateStability: false, coberturaReportFile: '**/build/reports/coverage.xml', conditionalCoverageTargets: '75, 50, 0', failUnhealthy: false, failUnstable: false, lineCoverageTargets: '75, 50, 0', maxNumberOfBuilds: 50, methodCoverageTargets: '75, 50, 0', onlyStable: false, zoomCoverageChart: false
     }
 
