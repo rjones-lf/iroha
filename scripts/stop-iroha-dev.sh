fbffcaa582ae229381e649bcc9791cdd650f35f3
Igor Egorov <igor@soramitsu.co.jp>
2018-03-22 18:23:51 +0300

    Improve scripts for docker environment management
    * run-iroha-dev.sh now automatically picks first free port from
      range 50051-50101 on host system for Iroha and free port from
      range 20000-20100 on host system for Debugger
    * run-iroha-dev.sh would not recreate containers in case of its rerun
      without prelimiary run of stop-iroha-dev.sh
    * Add stop-iroha-dev.sh that removes all iroha* containers and network
      which were created under current user.
    
    Signed-off-by: Igor Egorov <igor@soramitsu.co.jp>

--- scripts/stop-iroha-dev.sh
+++ scripts/stop-iroha-dev.sh
@@ -4,4 +4,8 @@ IROHA_HOME="$(dirname "${CURDIR}")"
 PROJECT=iroha${UID}
 COMPOSE=${IROHA_HOME}/docker/docker-compose.yml
 
+# actual values of are not needed here, but variables need to be defined
+export IROHA_PORT=1
+export DEBUGGER_PORT=2
+
 docker-compose -f ${COMPOSE} -p ${PROJECT} down
