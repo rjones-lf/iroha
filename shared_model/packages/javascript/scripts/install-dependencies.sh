a265390e3fc5d2de9816821a5f06671e330dbd29
Victor Drobny <vdrobny@yandex.ru>
2018-05-09 13:52:44 +0300

    fix pr issues
    Signed-off-by: Victor Drobny <vdrobny@yandex.ru>

--- shared_model/packages/javascript/scripts/install-dependencies.sh
+++ shared_model/packages/javascript/scripts/install-dependencies.sh
@@ -21,7 +21,8 @@ git clone https://github.com/boostorg/boost /tmp/boost; \
     rm -rf /tmp/boost
 
 # Protobuf (static) v3.5.1
-git clone --depth 1 --branch 106ffc04be1abf3ff3399f54ccf149815b287dd9 https://github.com/google/protobuf
+git clone --branch master https://github.com/google/protobuf
 cd protobuf
+git reset --hard 106ffc04be1abf3ff3399f54ccf149815b287dd9
 cmake -Hcmake/ -Bbuild -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_BUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_BUILD_TYPE=Release
 sudo cmake --build build/ --target install -- -j"$(getconf _NPROCESSORS_ONLN)"
