728c19d25732810e78dc5f069422457d22ab6a1d
Akvinikym <anarant12@gmail.com>
2018-06-05 17:50:07 +0300

    Separated query_responses in interfaces
    Signed-off-by: Akvinikym <anarant12@gmail.com>

--- shared_model/interfaces/query_responses/account_asset_response.hpp
+++ shared_model/interfaces/query_responses/account_asset_response.hpp
@@ -20,9 +20,6 @@
 
 #include "interfaces/base/model_primitive.hpp"
 #include "interfaces/common_objects/account_asset.hpp"
-#include "interfaces/common_objects/types.hpp"
-#include "utils/string_builder.hpp"
-#include "utils/visitor_apply_for_all.hpp"
 
 namespace shared_model {
   namespace interface {
@@ -36,25 +33,9 @@ namespace shared_model {
        */
       virtual const AccountAsset &accountAsset() const = 0;
 
-      /**
-       * Stringify the data.
-       * @return string representation of data.
-       */
-      std::string toString() const override {
-        return detail::PrettyStringBuilder()
-            .init("AccountAssetResponse")
-            .append(accountAsset().toString())
-            .finalize();
-      }
+      std::string toString() const override;
 
-      /**
-       * Implementation of operator ==
-       * @param rhs - the right-hand side of AccountAssetResponse object
-       * @return true if they are same.
-       */
-      bool operator==(const ModelType &rhs) const override {
-        return accountAsset() == rhs.accountAsset();
-      }
+      bool operator==(const ModelType &rhs) const override;
     };
   }  // namespace interface
 }  // namespace shared_model
---
+++ shared_model/interfaces/query_responses/impl/account_asset_response.cpp
@@ -0,0 +1,25 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "interfaces/query_responses/account_asset_response.hpp"
+#include "utils/visitor_apply_for_all.hpp"
+#include "utils/string_builder.hpp"
+
+namespace shared_model {
+  namespace interface {
+
+    std::string AccountAssetResponse::toString() const {
+      return detail::PrettyStringBuilder()
+          .init("AccountAssetResponse")
+          .append(accountAsset().toString())
+          .finalize();
+    }
+
+    bool AccountAssetResponse::operator==(const ModelType &rhs) const {
+      return accountAsset() == rhs.accountAsset();
+    }
+
+  }  // namespace interface
+}  // namespace shared_model
