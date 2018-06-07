407c1e3c9c2ec5d1cd831b0853174e4e6b2e3a1d
Kitsu <mail@kitsu.me>
2018-06-08 01:21:41 +0300

    Use new perms in RolePermResp
    Signed-off-by: Kitsu <mail@kitsu.me>

--- shared_model/builders/protobuf/builder_templates/query_response_template.hpp
+++ shared_model/builders/protobuf/builder_templates/query_response_template.hpp
@@ -84,8 +84,9 @@ namespace shared_model {
           iroha::protocol::AccountAssetResponse *query_response =
               proto_query_response.mutable_account_assets_response();
 
-          for (auto &asset: assets) {
-            query_response->add_account_assets()->CopyFrom(asset.getTransport());
+          for (auto &asset : assets) {
+            query_response->add_account_assets()->CopyFrom(
+                asset.getTransport());
           }
         });
       }
@@ -168,13 +169,17 @@ namespace shared_model {
       }
 
       auto rolePermissionsResponse(
-          const std::vector<interface::types::PermissionNameType>
-              &role_permissions) const {
+          const interface::RolePermissionSet &role_permissions) const {
         return queryResponseField([&](auto &proto_query_response) {
           iroha::protocol::RolePermissionsResponse *query_response =
               proto_query_response.mutable_role_permissions_response();
-          for (const auto &perm : role_permissions) {
-            query_response->add_permissions(perm);
+          for (size_t i = 0; i < role_permissions.size(); ++i) {
+            auto perm = static_cast<interface::permissions::Role>(i);
+            if (role_permissions[perm]) {
+              // TODO(@l4l) 05/06/18 IR-1393
+              // replace with toTransport after fixing RolePermissionResponse
+              query_response->add_permissions(permissions::toString(perm));
+            }
           }
 
           // TODO(@l4l) 05/06/18 IR-1393
