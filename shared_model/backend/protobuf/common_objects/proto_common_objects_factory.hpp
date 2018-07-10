e9049646a804e4a34e910fe10c861466b7694a8d
Nikita Alekseev <n.alekseev2104@gmail.com>
2018-07-10 14:18:11 +0300

    Add validation logic to factory
    This allows addition of validator in the future
    
    Signed-off-by: Nikita Alekseev <n.alekseev2104@gmail.com>
    
    # Conflicts:
    #	test/benchmark/CMakeLists.txt
    #	test/benchmark/bm_proto_creation.cpp

--- shared_model/backend/protobuf/common_objects/proto_common_objects_factory.hpp
+++ shared_model/backend/protobuf/common_objects/proto_common_objects_factory.hpp
@@ -10,6 +10,7 @@
 #include "common/result.hpp"
 #include "interfaces/common_objects/common_objects_factory.hpp"
 #include "primitive.pb.h"
+#include "validators/answer.hpp"
 
 namespace shared_model {
   namespace proto {
@@ -22,12 +23,27 @@ namespace shared_model {
         iroha::protocol::Peer peer;
         peer.set_address(address);
         peer.set_peer_key(crypto::toBinaryString(public_key));
+        auto proto_peer = std::make_unique<Peer>(std::move(peer));
+
+        shared_model::validation::Answer answer;
+
+        validation::ReasonsGroupType reasons;
+        this->validator_.validatePeer(reasons, *proto_peer);
+
+        if (not reasons.second.empty()) {
+          answer.addReason(std::move(reasons));
+        }
+
+        if (answer) {
+          return iroha::expected::makeError(answer.reason());
+        }
+
         return iroha::expected::makeValue<std::unique_ptr<interface::Peer>>(
-            std::make_unique<Peer>(std::move(peer)));
+            std::move(proto_peer));
       }
 
      private:
-      Validator validator;
+      Validator validator_;
     };
   }  // namespace proto
 }  // namespace shared_model
--- test/module/shared_model/backend_proto/common_objects/proto_common_objects_factory_test.cpp
+++ test/module/shared_model/backend_proto/common_objects/proto_common_objects_factory_test.cpp
@@ -3,29 +3,55 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "backend/protobuf/common_objects/proto_common_objects_factory.hpp"
-#include "framework/result_fixture.hpp"
-
 #include <gtest/gtest.h>
+
+#include "backend/protobuf/common_objects/proto_common_objects_factory.hpp"
+#include "cryptography/crypto_provider/crypto_defaults.hpp"
+#include "framework/result_fixture.hpp"
+#include "validators/field_validator.hpp"
 
 using namespace shared_model;
 using namespace framework::expected;
 
-TEST(PeerTest, ValidPeerInitialization) {
-  proto::ProtoCommonObjectsFactory<int> factory;
+class PeerTest : public ::testing::Test {
+ public:
+  std::string valid_address = "127.0.0.1:8080";
+  crypto::PublicKey valid_pubkey =
+      crypto::DefaultCryptoAlgorithmType::generateKeypair().publicKey();
+  std::string invalid_address = "127.0.0.1";
+};
 
-  auto peer = factory.createPeer(
-      "127.0.0.1",
-      interface::types::PubkeyType(
-          shared_model::crypto::Blob::fromHexString("1234")));
+/**
+ * @given valid data for peer
+ * @when peer is created via factory
+ * @then peer is successfully initialized
+ */
+TEST_F(PeerTest, ValidPeerInitialization) {
+  proto::ProtoCommonObjectsFactory<validation::FieldValidator> factory;
+
+  auto peer = factory.createPeer(valid_address, valid_pubkey);
 
   peer.match(
-      [](const ValueOf<decltype(peer)> &v) {
-        ASSERT_EQ(v.value->address(), "127.0.0.1");
-        ASSERT_EQ(v.value->pubkey().hex(), "1234");
+      [&](const ValueOf<decltype(peer)> &v) {
+        ASSERT_EQ(v.value->address(), valid_address);
+        ASSERT_EQ(v.value->pubkey().hex(), valid_pubkey.hex());
       },
-      [](const ErrorOf<decltype(peer)> &e) {
-        FAIL();
-      }
-  );
+      [](const ErrorOf<decltype(peer)> &e) { FAIL() << e.error; });
+}
+
+/**
+ * @given invalid data for peer
+ * @when peer is created via factory
+ * @then peer is not initialized correctly
+ */
+TEST_F(PeerTest, InvalidPeerInitialization) {
+  proto::ProtoCommonObjectsFactory<validation::FieldValidator> factory;
+
+  auto keypair = crypto::DefaultCryptoAlgorithmType::generateKeypair();
+
+  auto peer = factory.createPeer(invalid_address, keypair.publicKey());
+
+  peer.match(
+      [](const ValueOf<decltype(peer)> &v) { FAIL() << "Expected error case"; },
+      [](const ErrorOf<decltype(peer)> &e) { SUCCEED(); });
 }
