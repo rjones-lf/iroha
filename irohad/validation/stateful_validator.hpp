971dde3253f87d79aa263bfd2d1493d13cae0add
Nikita Alekseev <n.alekseev2104@gmail.com>
2018-07-20 18:39:19 +0300

    Add proposal factory to initialization where required
    Signed-off-by: Nikita Alekseev <n.alekseev2104@gmail.com>
    
    # Conflicts:
    #	shared_model/backend/protobuf/common_objects/proto_common_objects_factory.hpp
    #	shared_model/interfaces/common_objects/common_objects_factory.hpp
    #	test/module/shared_model/backend_proto/common_objects/proto_common_objects_factory_test.cpp

--- irohad/validation/stateful_validator.hpp
+++ irohad/validation/stateful_validator.hpp
@@ -38,7 +38,8 @@ namespace iroha {
        * this wsv not affected on ledger,
        * all changes after removing wsv will be ignored
        * @return proposal with valid transactions and errors, which appeared in
-       * a process of validating
+       * a process of validating.
+       * If, for some reason, proposal was not created, nullptr is returned
        */
       virtual VerifiedProposalAndErrors validate(
           const shared_model::interface::Proposal &proposal,
