5cd404ae127809e3c07511e68c8210af6bb72c5c
kamilsa <kamilsa16@gmail.com>
2018-07-25 21:09:33 +0300

    Update command service
    Signed-off-by: kamilsa <kamilsa16@gmail.com>

--- irohad/torii/impl/command_service.cpp
+++ irohad/torii/impl/command_service.cpp
@@ -136,28 +136,27 @@ namespace torii {
                 iroha::expected::Value<
                     shared_model::interface::TransactionSequence>
                     &tx_sequence) {
-              auto txs = tx_sequence.value.transactions();
-              std::for_each(txs.begin(), txs.end(), [this](auto &tx) {
-                auto tx_hash = tx->hash();
-                if (cache_->findItem(tx_hash) and tx->quorum() < 2) {
-                  log_->warn("Found transaction {} in cache, ignoring",
-                             tx_hash.hex());
-                  return;
-                }
+              for (const auto &batch : tx_sequence.value.batches()) {
+                tx_processor_->batchHandle(batch);
+                const auto &txs = batch.transactions();
+                std::for_each(txs.begin(), txs.end(), [this](const auto &tx) {
+                  auto tx_hash = tx->hash();
+                  if (cache_->findItem(tx_hash) and tx->quorum() < 2) {
+                    log_->warn("Found transaction {} in cache, ignoring",
+                               tx_hash.hex());
+                    return;
+                  }
 
-                // setting response
-                iroha::protocol::ToriiResponse response;
-                response.set_tx_hash(
-                    shared_model::crypto::toBinaryString(tx_hash));
-                response.set_tx_status(
-                    iroha::protocol::TxStatus::STATELESS_VALIDATION_SUCCESS);
-
-                // Send transaction to iroha
-                tx_processor_->transactionHandle(tx);
-
-                this->addTxToCacheAndLog(
-                    "ToriiList", std::move(tx_hash), std::move(response));
-              });
+                  // setting response
+                  iroha::protocol::ToriiResponse response;
+                  response.set_tx_hash(
+                      shared_model::crypto::toBinaryString(tx_hash));
+                  response.set_tx_status(
+                      iroha::protocol::TxStatus::STATELESS_VALIDATION_SUCCESS);
+                  this->addTxToCacheAndLog(
+                      "ToriiList", std::move(tx_hash), std::move(response));
+                });
+              }
             },
             [this, &tx_list](auto &error) {
               auto &txs = tx_list.transactions();
