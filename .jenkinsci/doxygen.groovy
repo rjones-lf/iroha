3a9d4f4a02543bad31e7976a0f0e176f46fd06ef
Alexey Rodionov <rodionov12@gmail.com>
2018-07-29 04:46:17 +0300

    Update for review
    Signed-off-by: Alexey Rodionov <rodionov12@gmail.com>

--- .jenkinsci/doxygen.groovy
+++ .jenkinsci/doxygen.groovy
@@ -2,7 +2,7 @@
 
 def doDoxygen() {
   try {
-    if (env.GIT_LOCAL_BRANCH ==~ /(master|develop)/ || env.CHANGE_BRANCH == 'develop') {
+    if (env.GIT_LOCAL_BRANCH in ["master","develop"] || env.CHANGE_BRANCH == 'develop') {
       def branch = env.CHANGE_BRANCH == 'develop' ? env.CHANGE_BRANCH : env.GIT_LOCAL_BRANCH
       sh "doxygen Doxyfile"
       sshagent(['jenkins-artifact']) {
@@ -12,7 +12,7 @@ def doDoxygen() {
           -e 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' \
           -rzcv --delete \
           docs/doxygen/html/* \
-          ubuntu@nexus.soramitsu.co.jp:/var/nexus-efs/doxygen/${branch}/
+          ubuntu@docs.iroha.tech:/var/nexus-efs/doxygen/${branch}/
         """
       }
     }
