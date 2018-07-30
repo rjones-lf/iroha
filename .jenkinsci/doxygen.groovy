51d42e982eb07d12603664782ba07d8fe298ca23
Alexey Rodionov <rodionov12@gmail.com>
2018-07-30 14:23:10 +0300

    Remove try-catch block
    Signed-off-by: Alexey Rodionov <rodionov12@gmail.com>

--- .jenkinsci/doxygen.groovy
+++ .jenkinsci/doxygen.groovy
@@ -1,23 +1,20 @@
 #!/usr/bin/env groovy
 
 def doDoxygen() {
-  try {
-    if (env.GIT_LOCAL_BRANCH in ["master","develop"] || env.CHANGE_BRANCH == 'develop') {
-      def branch = env.CHANGE_BRANCH == 'develop' ? env.CHANGE_BRANCH : env.GIT_LOCAL_BRANCH
-      sh "doxygen Doxyfile"
-      sshagent(['jenkins-artifact']) {
-        sh "ssh-agent"
-        sh """
-          rsync \
-          -e 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' \
-          -rzcv --delete \
-          docs/doxygen/html/* \
-          ubuntu@docs.iroha.tech:/var/nexus-efs/doxygen/${branch}/
-        """
-      }
+  if (env.GIT_LOCAL_BRANCH in ["master","develop"] || env.CHANGE_BRANCH == 'develop') {
+    def branch = env.CHANGE_BRANCH == 'develop' ? env.CHANGE_BRANCH : env.GIT_LOCAL_BRANCH
+    sh "doxygen Doxyfile"
+    sshagent(['jenkins-artifact']) {
+      sh "ssh-agent"
+      sh """
+        rsync \
+        -e 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' \
+        -rzcv --delete \
+        docs/doxygen/html/* \
+        ubuntu@docs.iroha.tech:/var/nexus-efs/doxygen/${branch}/
+      """
     }
   }
-  catch (MissingPropertyException e) {}
 }
 
 return this
