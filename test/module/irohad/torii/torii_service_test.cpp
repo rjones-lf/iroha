05b3146c6da5e022a390617e3c8e6a80be15e39a
Igor Egorov <igor@soramitsu.co.jp>
2018-07-30 11:49:47 +0300

    Fix typo
    Signed-off-by: Igor Egorov <igor@soramitsu.co.jp>

--- test/module/irohad/torii/torii_service_test.cpp
+++ test/module/irohad/torii/torii_service_test.cpp
@@ -34,7 +34,7 @@ using namespace iroha::torii;
 using namespace std::chrono_literals;
 constexpr std::chrono::milliseconds initial_timeout = 1s;
 constexpr std::chrono::milliseconds nonfinal_timeout = 2 * 10s;
-constexpr unsigned resubscibe_attempts = 3;
+constexpr unsigned resubscribe_attempts = 3;
 
 using iroha::Commit;
 
@@ -409,7 +409,7 @@ TEST_F(ToriiServiceTest, StreamingFullPipelineTest) {
   // (Committed in this case) will be received. We start request before
   // transaction sending so we need in a separate thread for it.
   std::thread t([&] {
-    unsigned resub_counter(resubscibe_attempts);
+    unsigned resub_counter(resubscribe_attempts);
     iroha::protocol::TxStatusRequest tx_request;
     tx_request.set_tx_hash(txhash);
     do {
