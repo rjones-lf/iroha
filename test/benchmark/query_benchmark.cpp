51274001b8d10687dfb153741fddcde139032694
Victor Drobny <vdrobny@yandex.ru>
2018-08-13 13:14:50 +0300

    refactoring
    Signed-off-by: Victor Drobny <vdrobny@yandex.ru>

--- test/benchmark/query_benchmark.cpp
+++ test/benchmark/query_benchmark.cpp
@@ -1,62 +1,24 @@
 /**
- * Copyright Soramitsu Co., Ltd. 2017 All Rights Reserved.
- * http://soramitsu.co.jp
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
  */
-
-///
-/// Documentation is at https://github.com/google/benchmark
-///
-/// General recommendations:
-///  - build with -DCMAKE_BUILD_TYPE=Release
-///  - disable CPU scaling (frequency changes depending on workload)
-///  - put initialization code in fixtures
-///  - write meaningful names for benchmarks
-///
 
 #include <benchmark/benchmark.h>
 #include <string>
 
 #include "backend/protobuf/transaction.hpp"
 #include "builders/protobuf/unsigned_proto.hpp"
-#include "cryptography/crypto_provider/crypto_defaults.hpp"
 #include "datetime/time.hpp"
 #include "framework/integration_framework/integration_test_framework.hpp"
 #include "module/shared_model/builders/protobuf/test_query_builder.hpp"
 #include "module/shared_model/builders/protobuf/test_transaction_builder.hpp"
-#include "validators/permissions.hpp"
 
 const std::string kUser = "user";
 const std::string kUserId = kUser + "@test";
-const std::string kAmount = "1.0";
 const shared_model::crypto::Keypair kAdminKeypair =
     shared_model::crypto::DefaultCryptoAlgorithmType::generateKeypair();
 const shared_model::crypto::Keypair kUserKeypair =
     shared_model::crypto::DefaultCryptoAlgorithmType::generateKeypair();
-
-TestUnsignedTransactionBuilder createUser(
-    const std::string &user, const shared_model::crypto::PublicKey &key) {
-  return TestUnsignedTransactionBuilder()
-      .createAccount(
-          user,
-          integration_framework::IntegrationTestFramework::kDefaultDomain,
-          key)
-      .creatorAccountId(
-          integration_framework::IntegrationTestFramework::kAdminId)
-      .createdTime(iroha::time::now())
-      .quorum(1);
-}
 
 TestUnsignedTransactionBuilder createUserWithPerms(
     const std::string &user,
@@ -65,16 +27,21 @@ TestUnsignedTransactionBuilder createUserWithPerms(
     const shared_model::interface::RolePermissionSet &perms) {
   const auto user_id = user + "@"
       + integration_framework::IntegrationTestFramework::kDefaultDomain;
-  return createUser(user, key)
+  return TestUnsignedTransactionBuilder()
+      .createAccount(
+          user,
+          integration_framework::IntegrationTestFramework::kDefaultDomain,
+          key)
+      .creatorAccountId(
+          integration_framework::IntegrationTestFramework::kAdminId)
+      .createdTime(iroha::time::now())
+      .quorum(1)
       .detachRole(user_id,
                   integration_framework::IntegrationTestFramework::kDefaultRole)
       .createRole(role_id, perms)
       .appendRole(user_id, role_id);
 }
 
-/// Test how long is empty std::string creation
-// define a static function, which accepts 'state'
-// function's name = benchmark's name
 static void BM_QueryAccount(benchmark::State &state) {
   integration_framework::IntegrationTestFramework itf(1);
   itf.setInitialState(kAdminKeypair);
