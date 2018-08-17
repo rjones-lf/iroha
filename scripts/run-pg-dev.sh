1c3406447530238345be8c5d24016676349c8400
tyvision <antykushin@gmail.com>
2018-08-17 09:27:45 +0300

    fix review comments (2)
    Signed-off-by: tyvision <antykushin@gmail.com>

--- scripts/run-pg-dev.sh
+++ scripts/run-pg-dev.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-export PG_CONTAINER_NAME="postgres"
+PG_CONTAINER_NAME="postgres"
 PG_IMAGE_NAME="postgres:9.5"
-PG_HOST_DATA="/tmp/pgdata"
 PG_CONTAINER_DATA="/var/lib/postgresql/data"
 PG_USER="iroha"
 PG_PASS="mysecretpassword"
-export PG_PORT="5432"
+PG_PORT="5432"
 
-export G_ID=$(id -g)
-export U_ID=$(id -u)
+G_ID=$(id -g)
+U_ID=$(id -u)
 
-mkdir -p ${PG_HOST_DATA}
+mkdir -p ${PG_CONTAINER_DATA}
 docker run -dt --user "${U_ID}:${G_ID}" \
         --name ${PG_CONTAINER_NAME} \
         -p 127.0.0.1:${PG_PORT}:${PG_PORT} \
-        -v ${PG_HOST_DATA}:${PG_CONTAINER_DATA} \
+        -v ${PG_CONTAINER_DATA} \
         -e POSTGRES_USER=${PG_USER} -e POSTGRES_PASSWORD=${PG_PASS} \
         ${PG_IMAGE_NAME}
 
-export PG_ADDR=`docker inspect --format '{''{ .NetworkSettings.IPAddress }''}' ${PG_CONTAINER_NAME}`
-echo "postgres is now available at ${PG_ADDR}:${PG_PORT}"
+echo "postgres is now available at 127.0.0.1:${PG_PORT}"
--- scripts/stop-pg-dev.sh
+++ scripts/stop-pg-dev.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export PG_CONTAINER_NAME="postgres"
-PG_HOST_DATA="/tmp/pgdata"
-export PG_PORT="5432"
+PG_CONTAINER_NAME="postgres"
+PG_CONTAINER_DATA="/var/lib/postgresql/data"
+PG_PORT="5432"
 
-docker stop ${PG_CONTAINER_NAME}; docker rm -f ${PG_CONTAINER_NAME}
-rm -rf ${PG_HOST_DATA}
+docker stop ${PG_CONTAINER_NAME}; docker rm ${PG_CONTAINER_NAME}
+rm -rf ${PG_CONTAINER_DATA}
 
 echo "postgres container is successfully stopped and removed from your OS"
