dcbeb20e01d9ed1d0be9c4f08d57257297eb38f0
kamilsa <kamilsa16@gmail.com>
2018-08-24 10:26:10 +0300

    Add comments, refactor ASSERTS
    Signed-off-by: kamilsa <kamilsa16@gmail.com>

--- test/integration/pipeline/batch_pipeline_test.cpp
+++ test/integration/pipeline/batch_pipeline_test.cpp
@@ -24,6 +24,10 @@ class BatchPipelineTest
     : public AcceptanceFixture,
       public WithParamInterface<interface::types::BatchType> {
  public:
+  /**
+   * Create transaction to create first user
+   * @return transaction to create first user
+   */
   auto createFirstUser() {
     return AcceptanceFixture::createUser(kFirstUser,
                                          kFirstUserKeypair.publicKey())
@@ -32,9 +36,11 @@ class BatchPipelineTest
         .finish();
   }
 
-  auto createSecondUser(const interface::RolePermissionSet &perms = {
-                            interface::permissions::Role::kTransfer,
-                            interface::permissions::Role::kAddAssetQty}) {
+  /**
+   * Create transaction to create second user
+   * @return transaction to create second user
+   */
+  auto createSecondUser() {
     return AcceptanceFixture::createUser(kSecondUser,
                                          kSecondUserKeypair.publicKey())
         .build()
@@ -42,6 +48,14 @@ class BatchPipelineTest
         .finish();
   }
 
+  /**
+   * Create transaction to create asset and add its given amount to given user
+   * @param account_id account for which amount of asset is added
+   * @param asset_name name of the asset to be created and added to the account
+   * @param amount amount of the asset to be added to the account
+   * @param keypair is used to sign transaction
+   * @return transaction with create asset and add asset quantity commands
+   */
   auto createAndAddAssets(std::string account_id,
                           std::string asset_name,
                           std::string amount,
@@ -57,6 +71,15 @@ class BatchPipelineTest
         .finish();
   }
 
+  /**
+   * Create builder for transaction to do transfer between given users with
+   * given amount of given asset
+   * @param src_account_id source accound id
+   * @param dest_account_id destination account id
+   * @param asset_name name of the asset (without domain) to be transferred
+   * @param amount amount of asset to be transferred
+   * @return transaction builder with transfer asset command
+   */
   auto prepareTransferAssetBuilder(const std::string &src_account_id,
                                    const std::string &dest_account_id,
                                    const std::string &asset_name,
@@ -72,6 +95,12 @@ class BatchPipelineTest
                        amount);
   }
 
+  /**
+   * Takes transaction and sign it wit provided signature
+   * @param tx to be signed
+   * @param keypair to sign
+   * @return signed transaction
+   */
   auto signedTx(std::shared_ptr<interface::Transaction> tx,
                 const crypto::Keypair &keypair) {
     auto signed_blob =
@@ -81,13 +110,21 @@ class BatchPipelineTest
     return std::shared_ptr<interface::Transaction>(std::move(clone_tx));
   }
 
- protected:
-  std::string createAccountId(const std::string &account_name) {
-    return account_name + "@" + kDomain;
+  auto createTransactionSequence(
+      const interface::types::SharedTxsCollectionType &txs) {
+    auto transaction_sequence_result =
+        interface::TransactionSequence::createTransactionSequence(
+            txs, validation::DefaultUnsignedTransactionsValidator());
+
+    auto transaction_sequence_value =
+        framework::expected::val(transaction_sequence_result);
+    EXPECT_TRUE(transaction_sequence_value)
+        << framework::expected::err(transaction_sequence_result).value().error;
+
+    return transaction_sequence_value.value().value;
   }
 
-  const std::string kRole = "roleone";
-
+ protected:
   const std::string kAdmin = "admin";
   const std::string kFirstUser = "first";
   const std::string kSecondUser = "second";
@@ -104,25 +141,33 @@ class BatchPipelineTest
   const std::string kAssetB = "euro";
 };
 
+/**
+ * Matchers to compare references against pointers
+ */
+
+MATCHER(RefAndPointerEq, "") {
+  return get<0>(arg) == *get<1>(arg);
+}
+
+MATCHER_P(RefAndPointerEq, arg1, "") {
+  return arg == *arg1;
+}
+
+/**
+ * @given any type of batch (ordered or atomic) with two transactions
+ * @when transactions are sent to iroha
+ * @then both transactions are committed
+ */
 TEST_P(BatchPipelineTest, ValidBatch) {
   auto batch_transactions = framework::batch::makeTestBatchTransactions(
       GetParam(),
       prepareTransferAssetBuilder(kFirstUserId, kSecondUserId, kAssetA, "1.0"),
       prepareTransferAssetBuilder(kSecondUserId, kFirstUserId, kAssetB, "1.0"));
 
-  auto transaction_sequence_result =
-      interface::TransactionSequence::createTransactionSequence(
-          interface::types::SharedTxsCollectionType{
-              signedTx(batch_transactions[0], kFirstUserKeypair),
-              signedTx(batch_transactions[1], kSecondUserKeypair)},
-          validation::DefaultUnsignedTransactionsValidator());
-
-  auto transaction_sequence_value =
-      framework::expected::val(transaction_sequence_result);
-  ASSERT_TRUE(transaction_sequence_value)
-      << framework::expected::err(transaction_sequence_result).value().error;
-
-  auto transaction_sequence = transaction_sequence_value.value().value;
+  SCOPED_TRACE("From valid batch test")
+  auto transaction_sequence = createTransactionSequence(
+      {signedTx(batch_transactions[0], kFirstUserKeypair),
+       signedTx(batch_transactions[1], kSecondUserKeypair)});
 
   integration_framework::IntegrationTestFramework(1)
       .setInitialState(kAdminKeypair)
@@ -140,14 +185,18 @@ TEST_P(BatchPipelineTest, ValidBatch) {
             // check that transactions from block are the same as transactions
             // from transaction sequence
             ASSERT_THAT(block->transactions(),
-                        Pointwise(Truly([](const auto &args) {
-                                    return get<0>(args) == *get<1>(args);
-                                  }),
+                        Pointwise(RefAndPointerEq(),
                                   transaction_sequence.transactions()));
           })
       .done();
 }
 
+/**
+ * @given atomic batch of two transactions, with one transaction being stateful
+ * invalid
+ * @when batch is sent to iroha
+ * @then no transaction is committed
+ */
 TEST_F(BatchPipelineTest, InvalidAtomicBatch) {
   auto batch_transactions = framework::batch::makeTestBatchTransactions(
       interface::types::BatchType::ATOMIC,
@@ -158,19 +207,10 @@ TEST_F(BatchPipelineTest, InvalidAtomicBatch) {
                                   "2.0")  // invalid tx due to too big transfer
   );
 
-  auto transaction_sequence_result =
-      interface::TransactionSequence::createTransactionSequence(
-          interface::types::SharedTxsCollectionType{
-              signedTx(batch_transactions[0], kFirstUserKeypair),
-              signedTx(batch_transactions[1], kSecondUserKeypair)},
-          validation::DefaultUnsignedTransactionsValidator());
-
-  auto transaction_sequence_value =
-      framework::expected::val(transaction_sequence_result);
-  ASSERT_TRUE(transaction_sequence_value)
-      << framework::expected::err(transaction_sequence_result).value().error;
-
-  auto transaction_sequence = transaction_sequence_value.value().value;
+  SCOPED_TRACE("From invalid atomic batch test")
+  auto transaction_sequence = createTransactionSequence(
+      {signedTx(batch_transactions[0], kFirstUserKeypair),
+       signedTx(batch_transactions[1], kSecondUserKeypair)});
 
   integration_framework::IntegrationTestFramework(1)
       .setInitialState(kAdminKeypair)
@@ -192,11 +232,9 @@ TEST_F(BatchPipelineTest, InvalidAtomicBatch) {
                         }
                       })
       .checkProposal([&transaction_sequence](const auto proposal) {
-        ASSERT_THAT(proposal->transactions(),
-                    Pointwise(Truly([](const auto &args) {
-                                return get<0>(args) == *get<1>(args);
-                              }),
-                              transaction_sequence.transactions()));
+        ASSERT_THAT(
+            proposal->transactions(),
+            Pointwise(RefAndPointerEq(), transaction_sequence.transactions()));
       })
       .checkVerifiedProposal([](const auto verified_proposal) {
         ASSERT_THAT(verified_proposal->transactions(), IsEmpty());
@@ -204,34 +242,28 @@ TEST_F(BatchPipelineTest, InvalidAtomicBatch) {
       .done();
 }
 
+/**
+ * @given ordered batch of three transactions, with one transaction being
+ * stateful invalid
+ * @when batch is sent to iroha
+ * @then all transactions except stateful invalid one are committed
+ */
 TEST_F(BatchPipelineTest, InvalidOrderedBatch) {
   auto batch_transactions = framework::batch::makeTestBatchTransactions(
       interface::types::BatchType::ORDERED,
       prepareTransferAssetBuilder(kFirstUserId, kSecondUserId, kAssetA, "0.3"),
-      prepareTransferAssetBuilder(kSecondUserId,
-                                  kFirstUserId,
-                                  kAssetB,
-                                  "2.0"),  // invalid tx due to too big transfer
+      prepareTransferAssetBuilder(
+          kSecondUserId,
+          kFirstUserId,
+          kAssetB,
+          "2.0"),  // stateful invalid tx due to too big transfer
       prepareTransferAssetBuilder(kFirstUserId, kSecondUserId, kAssetA, "0.7"));
 
-  auto transaction_sequence_result =
-      interface::TransactionSequence::createTransactionSequence(
-          interface::types::SharedTxsCollectionType{
-              signedTx(batch_transactions[0], kFirstUserKeypair),
-              signedTx(batch_transactions[1], kSecondUserKeypair),
-              signedTx(batch_transactions[2], kFirstUserKeypair)},
-          validation::DefaultUnsignedTransactionsValidator());
-
-  auto transaction_sequence_value =
-      framework::expected::val(transaction_sequence_result);
-  ASSERT_TRUE(transaction_sequence_value)
-      << framework::expected::err(transaction_sequence_result).value().error;
-
-  auto transaction_sequence = transaction_sequence_value.value().value;
-
-  auto Is = [](const auto tx) {
-    return [&tx](const auto &lhs) { return *tx == lhs; };
-  };
+  SCOPED_TRACE("From InvalidOrderedBatch");
+  auto transaction_sequence = createTransactionSequence(
+      {signedTx(batch_transactions[0], kFirstUserKeypair),
+       signedTx(batch_transactions[1], kSecondUserKeypair),
+       signedTx(batch_transactions[2], kFirstUserKeypair)});
 
   integration_framework::IntegrationTestFramework(1)
       .setInitialState(kAdminKeypair)
@@ -243,13 +275,15 @@ TEST_F(BatchPipelineTest, InvalidOrderedBatch) {
       .sendTxAwait(
           createAndAddAssets(kSecondUserId, kAssetB, "1.0", kSecondUserKeypair),
           [](const auto &) {})
-      .sendTxSequenceAwait(transaction_sequence,
-                           [&](const auto block) {
-                             ASSERT_THAT(
-                                 block->transactions(),
-                                 ElementsAre(Truly(Is(batch_transactions[0])),
-                                             Truly(Is(batch_transactions[2]))));
-                           })
+      .sendTxSequenceAwait(
+          transaction_sequence,
+          [&](const auto block) {
+            ASSERT_THAT(
+                block->transactions(),
+                ElementsAre(
+                    RefAndPointerEq(transaction_sequence.transactions()[0]),
+                    RefAndPointerEq(transaction_sequence.transactions()[2])));
+          })
       .done();
 }
 
