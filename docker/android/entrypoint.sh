c880c7d38420f8346aa20d67ef16ee4f2fe647d4
Kitsu <mail@kitsu.me>
2018-09-14 17:28:21 +0300

    Add extra space after bash header
    Signed-off-by: Kitsu <mail@kitsu.me>

--- docker/android/entrypoint.sh
+++ docker/android/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 export LIBP=lib
 case "$PLATFORM" in
   armeabi)
--- example/java/build_library.sh
+++ example/java/build_library.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+
 cd "$(dirname "$0")"
 
 # folder with bindings and native library
