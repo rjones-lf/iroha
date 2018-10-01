6b10dfaeae00fb14bebf4584911a00b3812c8f0a
kamilsa <kamilsa16@gmail.com>
2018-10-01 16:32:00 +0300

    Add test
    Signed-off-by: kamilsa <kamilsa16@gmail.com>

--- shared_model/validators/protobuf/proto_transaction_validator.hpp
+++ shared_model/validators/protobuf/proto_transaction_validator.hpp
@@ -17,7 +17,7 @@ namespace shared_model {
         : public TransactionValidator<FieldValidator, CommandValidator> {
      private:
       void validateProtoTx(const iroha::protocol::Transaction &transaction,
-                           ReasonsGroupType &reason) {
+                           ReasonsGroupType &reason) const {
         for (const auto &command :
              transaction.payload().reduced_payload().commands()) {
           if (command.command_case()
@@ -51,15 +51,20 @@ namespace shared_model {
           validateProtoTx(
               dynamic_cast<const proto::Transaction &>(tx).getTransport(),
               tx_reason);
-          auto interface_validation =
-              TransactionValidator<FieldValidator, CommandValidator>::validate(
-                  tx);
-          if (interface_validation.hasErrors()) {
-            tx_reason.second.push_back(interface_validation.reason());
-          }
         } catch (std::bad_cast &) {
           tx_reason.second.emplace_back(
               "Non proto transaction is sent to proto validator");
+        }
+        if (not tx_reason.second.empty()) {
+          answer.addReason(std::move(tx_reason));
+          return answer;
+        }
+        auto interface_validation =
+            TransactionValidator<FieldValidator, CommandValidator>::validate(
+                tx);
+
+        if (interface_validation.hasErrors()) {
+          tx_reason.second.push_back(interface_validation.reason());
         }
         if (not tx_reason.second.empty()) {
           answer.addReason(std::move(tx_reason));
---
+++ test/module/shared_model/validators/protobuf/proto_tx_validator_test.cpp
@@ -0,0 +1,131 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <gtest/gtest.h>
+
+#include "module/shared_model/builders/protobuf/test_transaction_builder.hpp"
+#include "module/shared_model/validators/validators_fixture.hpp"
+#include "validators/protobuf/proto_transaction_validator.hpp"
+
+class ProtoTxValidatorTest : public ValidatorsTest {
+ protected:
+  iroha::protocol::Transaction generateEmptyTransaction() {
+    std::string creator_account_id = "admin@test";
+
+    TestTransactionBuilder builder;
+    auto tx = builder.creatorAccountId(creator_account_id)
+                  .createdTime(created_time)
+                  .quorum(1)
+                  .build()
+                  .getTransport();
+    return tx;
+  }
+
+  iroha::protocol::Transaction generateCreateRoleTransaction(
+      const std::string &role_name,
+      iroha::protocol::RolePermission permission) {
+    auto tx = generateEmptyTransaction();
+
+    iroha::protocol::CreateRole cr;
+    cr.set_role_name(role_name);
+    cr.add_permissions(permission);
+
+    tx.mutable_payload()
+        ->mutable_reduced_payload()
+        ->add_commands()
+        ->mutable_create_role()
+        ->CopyFrom(cr);
+    return tx;
+  }
+};
+
+/**
+ * @given iroha::protocol::Transaction with defined command
+ * @when it is validated
+ * @then answer with no errors is returned
+ */
+TEST_F(ProtoTxValidatorTest, CommandIsSet) {
+  auto tx = generateEmptyTransaction();
+  // add not set command
+  iroha::protocol::CreateDomain cd;
+  cd.set_domain_id(domain_id);
+  cd.set_default_role(role_name);
+
+  tx.mutable_payload()
+      ->mutable_reduced_payload()
+      ->add_commands()
+      ->mutable_create_domain()
+      ->CopyFrom(cd);
+  shared_model::proto::Transaction proto_tx(tx);
+
+  shared_model::validation::ProtoTransactionValidator<
+      shared_model::validation::FieldValidator,
+      shared_model::validation::CommandValidatorVisitor<
+          shared_model::validation::FieldValidator>>
+      validator;
+  auto answer = validator.validate(proto_tx);
+  ASSERT_FALSE(answer.hasErrors()) << answer.reason();
+}
+
+/**
+ * @given iroha::protocol::Transaction with undefined command
+ * @when it is validated
+ * @then answer with errors is returned
+ */
+TEST_F(ProtoTxValidatorTest, CommandNotSet) {
+  auto tx = generateEmptyTransaction();
+  // add not set command
+  tx.mutable_payload()->mutable_reduced_payload()->add_commands();
+  shared_model::proto::Transaction proto_tx(tx);
+
+  shared_model::validation::ProtoTransactionValidator<
+      shared_model::validation::FieldValidator,
+      shared_model::validation::CommandValidatorVisitor<
+          shared_model::validation::FieldValidator>>
+      validator;
+  auto answer = validator.validate(proto_tx);
+  ASSERT_TRUE(answer.hasErrors());
+}
+
+/**
+ * @given iroha::protocol::Transaction containing create role transaction with
+ * valid role permission
+ * @when it is validated
+ * @then answer with errors is returned
+ */
+TEST_F(ProtoTxValidatorTest, CreateRoleValid) {
+  auto tx = generateCreateRoleTransaction(
+      role_name, iroha::protocol::RolePermission::can_read_assets);
+
+  shared_model::proto::Transaction proto_tx(tx);
+
+  shared_model::validation::ProtoTransactionValidator<
+      shared_model::validation::FieldValidator,
+      shared_model::validation::CommandValidatorVisitor<
+          shared_model::validation::FieldValidator>>
+      validator;
+  auto answer = validator.validate(proto_tx);
+  ASSERT_FALSE(answer.hasErrors());
+}
+
+/**
+ * @given iroha::protocol::Transaction
+ * @when it is validated
+ * @then answer with no errors is returned
+ */
+TEST_F(ProtoTxValidatorTest, CreateRoleInvalid) {
+  auto tx = generateCreateRoleTransaction(
+      role_name, static_cast<iroha::protocol::RolePermission>(-1));
+
+  shared_model::proto::Transaction proto_tx(tx);
+
+  shared_model::validation::ProtoTransactionValidator<
+      shared_model::validation::FieldValidator,
+      shared_model::validation::CommandValidatorVisitor<
+          shared_model::validation::FieldValidator>>
+      validator;
+  auto answer = validator.validate(proto_tx);
+  ASSERT_TRUE(answer.hasErrors());
+}
