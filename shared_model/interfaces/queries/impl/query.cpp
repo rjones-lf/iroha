d1f379d1a777590ab9ab336c159f6fed6e335f72
Victor Drobny <vdrobny@yandex.ru>
2018-10-10 17:49:08 +0200

    clang-format
    Signed-off-by: Victor Drobny <vdrobny@yandex.ru>

--- shared_model/interfaces/queries/impl/query.cpp
+++ shared_model/interfaces/queries/impl/query.cpp
@@ -3,22 +3,22 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include "interfaces/queries/query.hpp"
 #include "interfaces/queries/get_account.hpp"
 #include "interfaces/queries/get_account_asset_transactions.hpp"
 #include "interfaces/queries/get_account_assets.hpp"
 #include "interfaces/queries/get_account_detail.hpp"
 #include "interfaces/queries/get_account_transactions.hpp"
 #include "interfaces/queries/get_asset_info.hpp"
+#include "interfaces/queries/get_pending_transactions.hpp"
 #include "interfaces/queries/get_role_permissions.hpp"
 #include "interfaces/queries/get_roles.hpp"
 #include "interfaces/queries/get_signatories.hpp"
 #include "interfaces/queries/get_transactions.hpp"
-#include "interfaces/queries/get_pending_transactions.hpp"
-#include "interfaces/queries/query.hpp"
 #include "interfaces/queries/query_payload_meta.hpp"
 #include "utils/visitor_apply_for_all.hpp"
 
-using Variant = shared_model::interface::Query::QueryVariantType ;
+using Variant = shared_model::interface::Query::QueryVariantType;
 template Variant::~variant();
 template Variant::variant(Variant &&);
 template bool Variant::operator==(const Variant &) const;
