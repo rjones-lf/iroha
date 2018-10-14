37052336855803fd21a0aea1cb0be84824545c1f
Bogdan Vaneev <warchantua@gmail.com>
2018-10-15 00:27:01 +0300

    Add healthcheck
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

--- docker/release/run-iroha.sh
+++ docker/release/run-iroha.sh
@@ -7,9 +7,10 @@ CONFIG_PATH=${CONFIG_PATH:-config.docker}
 POSTGRES_PORT=${POSTGRES_PORT:-5432}
 BLOCKS_PATH=${BLOCKS_PATH:-`cat ${CONFIG_PATH} | jq -r .block_store_path`}
 POSTGRES_HOST=${POSTGRES_HOST:-`cat ${CONFIG_PATH} | jq -r .pg_opt | cut -d' ' -f1 | cut -d'=' -f2`}
+WAIT_TIMEOUT=${WAIT_TIMEOUT:-10}
 
 # postgres host is always defined
-/wait-for-it.sh -h $POSTGRES_HOST -p $POSTGRES_PORT -t 10 -- true
+/wait-for-it.sh -h $POSTGRES_HOST -p $POSTGRES_PORT -t ${WAIT_TIMEOUT} -- true
 
 if [ -z "$(ls -A ${BLOCKS_PATH})" ]; then
   # if ledger is empty then init ledger with genesis_block
