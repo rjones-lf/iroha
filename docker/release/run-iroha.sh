c74477eac00cba646be222e5a99d08e84b0fbf7f
Bogdan Vaneev <warchantua@gmail.com>
2018-10-14 17:42:18 +0300

    Change algorithm to autodetect pg host, set wait for pg 10 sec
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

--- docker/release/run-iroha.sh
+++ docker/release/run-iroha.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-set -xe
+set -ex
 
 GENESISBLOCK_PATH=${GENESISBLOCK_PATH:-genesis.block}
 CONFIG_PATH=${CONFIG_PATH:-config.docker}
 POSTGRES_PORT=${POSTGRES_PORT:-5432}
-BLOCKS_PATH=${BLOCKS_PATH:-`cat ${CONFIG_PATH} | jq -r .pg_opt | grep -Eo "host=(.*?)\s" | cut -d'=' -f2 | xargs`}
-POSTGRES_HOST=${POSTGRES_HOST:-`cat ${CONFIG_PATH} | jq -r .block_store_path`}
+BLOCKS_PATH=${BLOCKS_PATH:-`cat ${CONFIG_PATH} | jq -r .block_store_path`}
+POSTGRES_HOST=${POSTGRES_HOST:-`cat ${CONFIG_PATH} | jq -r .pg_opt | cut -d' ' -f1 | cut -d'=' -f2`}
 
 # postgres host is always defined
-/wait-for-it.sh -h ${POSTGRES_HOST} -p ${POSTGRES_PORT} -t 30 -- true
+/wait-for-it.sh -h $POSTGRES_HOST -p $POSTGRES_PORT -t 10 -- true
 
 if [ -z "$(ls -A ${BLOCKS_PATH})" ]; then
   # if ledger is empty then init ledger with genesis_block
