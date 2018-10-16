5af9e8114243b9998cb31f6aaeedff6998aba1a2
Akvinikym <anarant12@gmail.com>
2018-10-16 11:22:08 +0300

    Allowed empty blocks to be validated
    Signed-off-by: Akvinikym <anarant12@gmail.com>

--- shared_model/validators/transactions_collection/transactions_collection_validator.cpp
+++ shared_model/validators/transactions_collection/transactions_collection_validator.cpp
@@ -19,22 +19,28 @@
 namespace shared_model {
   namespace validation {
 
-    template <typename TransactionValidator>
-    TransactionsCollectionValidator<TransactionValidator>::
+    template <typename TransactionValidator, bool CollectionCanBeEmpty>
+    TransactionsCollectionValidator<TransactionValidator,
+                                    CollectionCanBeEmpty>::
         TransactionsCollectionValidator(
             const TransactionValidator &transactions_validator)
         : transaction_validator_(transactions_validator) {}
 
-    template <typename TransactionValidator>
+    template <typename TransactionValidator, bool CollectionCanBeEmpty>
     template <typename Validator>
-    Answer TransactionsCollectionValidator<TransactionValidator>::validateImpl(
-        const interface::types::TransactionsForwardCollectionType &transactions,
-        Validator &&validator) const {
+    Answer TransactionsCollectionValidator<TransactionValidator,
+                                           CollectionCanBeEmpty>::
+        validateImpl(const interface::types::TransactionsForwardCollectionType
+                         &transactions,
+                     Validator &&validator) const {
       Answer res;
       ReasonsGroupType reason;
       reason.first = "Transaction list";
 
       if (boost::empty(transactions)) {
+        if (CollectionCanBeEmpty) {
+          return res;
+        }
         reason.second.emplace_back("Transaction sequence can not be empty");
         res.addReason(std::move(reason));
         return res;
@@ -56,51 +62,67 @@ namespace shared_model {
       return res;
     }
 
-    template <typename TransactionValidator>
-    Answer TransactionsCollectionValidator<TransactionValidator>::validate(
-        const shared_model::interface::types::TransactionsForwardCollectionType
-            &transactions) const {
+    template <typename TransactionValidator, bool CollectionCanBeEmpty>
+    Answer TransactionsCollectionValidator<TransactionValidator,
+                                           CollectionCanBeEmpty>::
+        validate(const shared_model::interface::types::
+                     TransactionsForwardCollectionType &transactions) const {
       return validateImpl(transactions, [this](const auto &tx) {
         return transaction_validator_.validate(tx);
       });
     }
 
-    template <typename TransactionValidator>
-    Answer TransactionsCollectionValidator<TransactionValidator>::validate(
-        const shared_model::interface::types::SharedTxsCollectionType
-            &transactions) const {
+    template <typename TransactionValidator, bool CollectionCanBeEmpty>
+    Answer TransactionsCollectionValidator<TransactionValidator,
+                                           CollectionCanBeEmpty>::
+        validate(const shared_model::interface::types::SharedTxsCollectionType
+                     &transactions) const {
       return validate(transactions | boost::adaptors::indirected);
     }
 
-    template <typename TransactionValidator>
-    Answer TransactionsCollectionValidator<TransactionValidator>::validate(
-        const interface::types::TransactionsForwardCollectionType &transactions,
-        interface::types::TimestampType current_timestamp) const {
+    template <typename TransactionValidator, bool CollectionCanBeEmpty>
+    Answer TransactionsCollectionValidator<TransactionValidator,
+                                           CollectionCanBeEmpty>::
+        validate(const interface::types::TransactionsForwardCollectionType
+                     &transactions,
+                 interface::types::TimestampType current_timestamp) const {
       return validateImpl(
           transactions, [this, current_timestamp](const auto &tx) {
             return transaction_validator_.validate(tx, current_timestamp);
           });
     }
 
-    template <typename TransactionValidator>
-    Answer TransactionsCollectionValidator<TransactionValidator>::validate(
-        const interface::types::SharedTxsCollectionType &transactions,
-        interface::types::TimestampType current_timestamp) const {
+    template <typename TransactionValidator, bool CollectionCanBeEmpty>
+    Answer TransactionsCollectionValidator<TransactionValidator,
+                                           CollectionCanBeEmpty>::
+        validate(const interface::types::SharedTxsCollectionType &transactions,
+                 interface::types::TimestampType current_timestamp) const {
       return validate(transactions | boost::adaptors::indirected,
                       current_timestamp);
     }
 
-    template <typename TransactionValidator>
+    template <typename TransactionValidator, bool CollectionCanBeEmpty>
     const TransactionValidator &TransactionsCollectionValidator<
-        TransactionValidator>::getTransactionValidator() const {
+        TransactionValidator,
+        CollectionCanBeEmpty>::getTransactionValidator() const {
       return transaction_validator_;
     }
 
     template class TransactionsCollectionValidator<
-        DefaultUnsignedTransactionValidator>;
+        DefaultUnsignedTransactionValidator,
+        true>;
 
     template class TransactionsCollectionValidator<
-        DefaultSignedTransactionValidator>;
+        DefaultUnsignedTransactionValidator,
+        false>;
+
+    template class TransactionsCollectionValidator<
+        DefaultSignedTransactionValidator,
+        true>;
+
+    template class TransactionsCollectionValidator<
+        DefaultSignedTransactionValidator,
+        false>;
 
   }  // namespace validation
 }  // namespace shared_model
