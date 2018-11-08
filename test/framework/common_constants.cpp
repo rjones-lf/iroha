c2e04ad6ee700ee6596db3126b12dc999506cb54
Mikhail Boldyrev <miboldyrev@gmail.com>
2018-11-08 10:33:31 +0300

    remove comments' odd docstringness
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>

--- test/framework/common_constants.cpp
+++ test/framework/common_constants.cpp
@@ -11,31 +11,31 @@ using namespace shared_model::crypto;
 
 namespace common_constants {
 
-  /// user names
+  // user names
   const std::string kAdminName = "admin";
   const std::string kUser = "user";
   const std::string kSpectator = "spectator";
 
-  /// role names
+  // role names
   const std::string kAdminRole = "admin_role";
   const std::string kDefaultRole = "default_role";
   const std::string kRole = "user_role";
 
-  /// asset names
+  // asset names
   const std::string kAssetName = "coin";
 
-  /// domain names
+  // domain names
   const std::string kDomain = "domain";
   const std::string kSecondDomain = "domain2";
 
-  /// ids
+  // ids
   const std::string kAdminId = kAdminName + "@" + kDomain;
   const std::string kUserId = kUser + "@" + kDomain;
   const std::string kCloseSpectatorId = kSpectator + "@" + kDomain;
   const std::string kRemoteSpectatorId = kSpectator + "@" + kSecondDomain;
   const std::string kAssetId = kAssetName + "#" + kDomain;
 
-  /// keypairs
+  // keypairs
   const Keypair kAdminKeypair = DefaultCryptoAlgorithmType::generateKeypair();
   const Keypair kUserKeypair = DefaultCryptoAlgorithmType::generateKeypair();
   const Keypair kCloseSpectatorKeypair = DefaultCryptoAlgorithmType::generateKeypair();
