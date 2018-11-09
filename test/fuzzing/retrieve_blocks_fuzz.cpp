62fd2876ce7b2390a1f96d2004fabd27f12a99c4
Konstantin Munichev <toobwn@gmail.com>
2018-11-09 16:02:58 +0300

    Use ServerWriter mock instead of nullptr
    Signed-off-by: Konstantin Munichev <toobwn@gmail.com>

--- test/fuzzing/retrieve_blocks_fuzz.cpp
+++ test/fuzzing/retrieve_blocks_fuzz.cpp
@@ -5,6 +5,17 @@
 
 #include "block_loader_fixture.hpp"
 
+namespace fuzzing {
+  class MockServerWriter
+      : public grpc::ServerWriterInterface<iroha::protocol::Block> {
+    MOCK_METHOD1(Write, void(iroha::protocol::Block));
+    MOCK_METHOD2(Write,
+                 bool(const iroha::protocol::Block &, grpc::WriteOptions));
+    MOCK_METHOD0(SendInitialMetadata, void());
+    MOCK_METHOD1(NextMessageSize, bool(uint32_t *));
+  };
+}  // namespace fuzzing
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, std::size_t size) {
   static fuzzing::BlockLoaderFixture fixture;
 
@@ -15,7 +26,12 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, std::size_t size) {
   iroha::network::proto::BlocksRequest request;
   if (protobuf_mutator::libfuzzer::LoadProtoInput(true, data, size, &request)) {
     grpc::ServerContext context;
-    fixture.block_loader_service_->retrieveBlocks(&context, &request, nullptr);
+    fuzzing::MockServerWriter serverWriter;
+    fixture.block_loader_service_->retrieveBlocks(
+        &context,
+        &request,
+        reinterpret_cast<grpc::ServerWriter<iroha::protocol::Block> *>(
+            &serverWriter));
   }
 
   return 0;
