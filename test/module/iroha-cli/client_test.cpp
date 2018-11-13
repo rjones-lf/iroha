e86703daaf08c8b2e045f45bde3f4ba46f9740a3
Akvinikym <anarant12@gmail.com>
2018-11-13 15:13:57 +0300

    Stateful Validation Error Codes in Endpoint (#1821)
    * Introduced error codes to SQL
    
    Signed-off-by: Akvinikym <anarant12@gmail.com>
    
    * Builds
    
    Signed-off-by: Akvinikym <anarant12@gmail.com>
    
    * 4/16 commands
    
    Signed-off-by: Akvinikym <anarant12@gmail.com>
    
    * 13/16 done
    
    Signed-off-by: Akvinikym <anarant12@gmail.com>
    
    * Tests are finished
    
    Signed-off-by: Akvinikym <anarant12@gmail.com>
    
    * Review issues
    
    Signed-off-by: Akvinikym <anarant12@gmail.com>
    
    * Introduced error codes to SFV
    
    Signed-off-by: Akvinikym <anarant12@gmail.com>
    
    * Is now built
    
    Signed-off-by: Akvinikym <anarant12@gmail.com>
    
    * Error code is introduced, and tests are up
    
    Signed-off-by: Akvinikym <anarant12@gmail.com>
    
    * Added a separated function for parsing SQL errors and making an error
    
    Signed-off-by: Akvinikym <anarant12@gmail.com>
    
    * Review issues are addressed
    
    Signed-off-by: Akvinikym <anarant12@gmail.com>
    
    * Added ToDos
    
    Signed-off-by: Akvinikym <anarant12@gmail.com>
    
    * Some after-POW issues
    
    Signed-off-by: Akvinikym <anarant12@gmail.com>
    
    * Error codes has good ordering
    
    Signed-off-by: Akvinikym <anarant12@gmail.com>
    
    * More fixes
    
    Signed-off-by: Akvinikym <anarant12@gmail.com>
    
    * Build fix
    
    Signed-off-by: Akvinikym <anarant12@gmail.com>
    
    * And more
    
    Signed-off-by: Akvinikym <anarant12@gmail.com>
    
    * Refactored switch-case
    
    Signed-off-by: Akvinikym <anarant12@gmail.com>
    
    * Review issues are fixed
    
    Signed-off-by: Akvinikym <anarant12@gmail.com>
    
    * Little issue
    
    Signed-off-by: Akvinikym <anarant12@gmail.com>
    
    * Another issue
    
    Signed-off-by: Akvinikym <anarant12@gmail.com>

--- test/module/iroha-cli/client_test.cpp
+++ test/module/iroha-cli/client_test.cpp
@@ -105,7 +105,10 @@ class ClientServerTest : public testing::Test {
     auto status_bus = std::make_shared<iroha::torii::StatusBusImpl>();
     auto tx_processor =
         std::make_shared<iroha::torii::TransactionProcessorImpl>(
-            pcsMock, mst, status_bus);
+            pcsMock,
+            mst,
+            status_bus,
+            std::make_shared<shared_model::proto::ProtoTxStatusFactory>());
 
     auto pb_tx_factory =
         std::make_shared<iroha::model::converters::PbTransactionFactory>();
@@ -255,7 +258,7 @@ TEST_F(ClientServerTest, SendTxWhenStatelessInvalid) {
            and --read_attempt_counter);
   ASSERT_EQ(answer.tx_status(),
             iroha::protocol::TxStatus::STATELESS_VALIDATION_FAILED);
-  ASSERT_NE(answer.error_message().size(), 0);
+  ASSERT_NE(answer.err_or_cmd_name().size(), 0);
 }
 
 /**
@@ -292,6 +295,9 @@ TEST_F(ClientServerTest, SendTxWhenStatefulInvalid) {
   ASSERT_EQ(client.sendTx(tx).answer, iroha_cli::CliClient::OK);
 
   // fail the tx
+  auto cmd_name = "CommandName";
+  size_t cmd_index = 2;
+  uint32_t error_code = 3;
   auto verified_proposal = std::make_shared<shared_model::proto::Proposal>(
       TestProposalBuilder().height(0).createdTime(iroha::time::now()).build());
   verified_prop_notifier.get_subscriber().on_next(
@@ -299,12 +305,8 @@ TEST_F(ClientServerTest, SendTxWhenStatefulInvalid) {
           std::make_pair(verified_proposal,
                          iroha::validation::TransactionsErrors{std::make_pair(
                              iroha::validation::CommandError{
-                                 "CommandName", 2, true, 2},
+                                 cmd_name, error_code, true, cmd_index},
                              tx.hash())})));
-  auto stringified_error = "Stateful validation error in transaction "
-                           + tx.hash().hex() + ": command 'CommandName' with "
-                                               "index '2' did not pass verification with "
-                                               "error code '2'";
 
   auto getAnswer = [&]() {
     return client.getTxStatus(shared_model::crypto::toBinaryString(tx.hash()))
@@ -320,7 +322,9 @@ TEST_F(ClientServerTest, SendTxWhenStatefulInvalid) {
            and --read_attempt_counter);
   ASSERT_EQ(answer.tx_status(),
             iroha::protocol::TxStatus::STATEFUL_VALIDATION_FAILED);
-  ASSERT_EQ(answer.error_message(), stringified_error);
+  ASSERT_EQ(answer.err_or_cmd_name(), cmd_name);
+  ASSERT_EQ(answer.failed_cmd_index(), cmd_index);
+  ASSERT_EQ(answer.error_code(), error_code);
 }
 
 TEST_F(ClientServerTest, SendQueryWhenInvalidJson) {
--- test/module/irohad/torii/torii_service_test.cpp
+++ test/module/irohad/torii/torii_service_test.cpp
@@ -115,7 +115,10 @@ class ToriiServiceTest : public testing::Test {
     auto status_bus = std::make_shared<iroha::torii::StatusBusImpl>();
     auto tx_processor =
         std::make_shared<iroha::torii::TransactionProcessorImpl>(
-            pcsMock, mst, status_bus);
+            pcsMock,
+            mst,
+            status_bus,
+            std::make_shared<shared_model::proto::ProtoTxStatusFactory>());
 
     EXPECT_CALL(*block_query, getTxByHashSync(_))
         .WillRepeatedly(Return(boost::none));
@@ -325,14 +328,14 @@ TEST_F(ToriiServiceTest, StatusWhenBlocking) {
           .createdTime(iroha::time::now())
           .transactions(txs)
           .build());
+
+  auto cmd_name = "FailedCommand";
+  size_t cmd_index = 2;
+  uint32_t error_code = 3;
   auto errors = iroha::validation::TransactionsErrors{std::make_pair(
-      iroha::validation::CommandError{
-          "FailedCommand", 2, true, 2},
+      iroha::validation::CommandError{cmd_name, error_code, true, cmd_index},
       failed_tx_hash)};
-  auto stringified_error = "Stateful validation error in transaction "
-                           + failed_tx_hash.hex() + ": "
-                           "command 'FailedCommand' with index '2' "
-                           "did not pass verification with error code '2'";
+
   verified_prop_notifier_.get_subscriber().on_next(
       std::make_shared<iroha::validation::VerifiedProposalAndErrors>(
           std::make_pair(verified_proposal, errors)));
@@ -390,7 +393,9 @@ TEST_F(ToriiServiceTest, StatusWhenBlocking) {
   client5.Status(last_tx_request, stful_invalid_response);
   ASSERT_EQ(stful_invalid_response.tx_status(),
             iroha::protocol::TxStatus::STATEFUL_VALIDATION_FAILED);
-  ASSERT_EQ(stful_invalid_response.error_message(), stringified_error);
+  ASSERT_EQ(stful_invalid_response.err_or_cmd_name(), cmd_name);
+  ASSERT_EQ(stful_invalid_response.failed_cmd_index(), cmd_index);
+  ASSERT_EQ(stful_invalid_response.error_code(), error_code);
 }
 
 /**
@@ -630,7 +635,7 @@ TEST_F(ToriiServiceTest, FailedListOfTxs) {
 
         ASSERT_EQ(toriiResponse.tx_status(),
                   iroha::protocol::TxStatus::STATELESS_VALIDATION_FAILED);
-        auto msg = toriiResponse.error_message();
+        auto msg = toriiResponse.err_or_cmd_name();
         ASSERT_THAT(msg, HasSubstr("bad timestamp: sent from future"));
       });
 }
