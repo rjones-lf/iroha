47d97fb5920e197b95bfab9d7340260b8889c529
Konstantin Munichev <toobwn@gmail.com>
2018-11-13 16:07:54 +0300

    Move MockServerWriter to grpc_mocks.hpp
    Signed-off-by: Konstantin Munichev <toobwn@gmail.com>

--- test/fuzzing/retrieve_blocks_fuzz.cpp
+++ test/fuzzing/retrieve_blocks_fuzz.cpp
@@ -4,17 +4,7 @@
  */
 
 #include "block_loader_fixture.hpp"
-
-namespace fuzzing {
-  class MockServerWriter
-      : public grpc::ServerWriterInterface<iroha::protocol::Block> {
-    MOCK_METHOD1(Write, void(iroha::protocol::Block));
-    MOCK_METHOD2(Write,
-                 bool(const iroha::protocol::Block &, grpc::WriteOptions));
-    MOCK_METHOD0(SendInitialMetadata, void());
-    MOCK_METHOD1(NextMessageSize, bool(uint32_t *));
-  };
-}  // namespace fuzzing
+#include "module/vendor/grpc_mocks.hpp"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, std::size_t size) {
   static fuzzing::BlockLoaderFixture fixture;
@@ -26,7 +16,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, std::size_t size) {
   iroha::network::proto::BlocksRequest request;
   if (protobuf_mutator::libfuzzer::LoadProtoInput(true, data, size, &request)) {
     grpc::ServerContext context;
-    fuzzing::MockServerWriter serverWriter;
+    testing::MockServerWriter serverWriter;
     fixture.block_loader_service_->retrieveBlocks(
         &context,
         &request,
