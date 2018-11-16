c7662f92a1356244fa5c6159b40df76c6fe2ef5f
Igor Egorov <igor@soramitsu.co.jp>
2018-11-16 12:53:56 +0300

    Apply clang-format for transport test (old code wasn't formatted)
    Signed-off-by: Igor Egorov <igor@soramitsu.co.jp>

--- test/module/irohad/multi_sig_transactions/transport_test.cpp
+++ test/module/irohad/multi_sig_transactions/transport_test.cpp
@@ -87,13 +87,13 @@ TEST(TransportTest, SendAndReceive) {
   // we want to ensure that server side will call onNewState()
   // with same parameters as on the client side
   EXPECT_CALL(*notifications, onNewState(_, _))
-      .WillOnce(Invoke(
-          [&my_key, &cv, &state](const auto &from_key, auto const &target_state) {
-            EXPECT_EQ(my_key.publicKey(), from_key);
+      .WillOnce(Invoke([&my_key, &cv, &state](const auto &from_key,
+                                              auto const &target_state) {
+        EXPECT_EQ(my_key.publicKey(), from_key);
 
-            EXPECT_EQ(state, target_state);
-            cv.notify_one();
-          }));
+        EXPECT_EQ(state, target_state);
+        cv.notify_one();
+      }));
 
   transport->sendState(*peer, state);
   std::unique_lock<std::mutex> lock(mtx);
