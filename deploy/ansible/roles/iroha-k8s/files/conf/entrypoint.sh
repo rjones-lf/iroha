e0387bbec4bcc59cffe0d8ef0f751a2c1fb2530d
bagf <rory@eishgaming.co.za>
2018-11-23 22:41:13 +0200

    Cleaned up Postgres probe commands; Made Iroha docker image configurable; Removed test tail command from init container
    Signed-off-by: bagf <rory@eishgaming.co.za>

--- deploy/ansible/roles/iroha-k8s/files/conf/entrypoint.sh
+++ deploy/ansible/roles/iroha-k8s/files/conf/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# Wait for Postgres container
+sleep 10
 if [ -z "$(ls -A /opt/block_store)" ]; then
 	irohad --genesis_block genesis.block --config config.sample --keypair_name node
 else
