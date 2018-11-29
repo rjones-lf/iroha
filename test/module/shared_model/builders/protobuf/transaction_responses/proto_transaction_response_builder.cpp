59f84c326483c759ab2909152a266ee7f2dbb459
Akvinikym <anarant12@gmail.com>
2018-11-29 11:23:35 +0300

    Fixed little thing
    Signed-off-by: Akvinikym <anarant12@gmail.com>

--- test/module/shared_model/builders/protobuf/transaction_responses/proto_transaction_response_builder.cpp
+++ test/module/shared_model/builders/protobuf/transaction_responses/proto_transaction_response_builder.cpp
@@ -64,7 +64,7 @@ TYPED_TEST(ProtoTransactionStatusBuilderTest, TestStatusType) {
                       .txHash(shared_model::crypto::Hash(expected_hash))
                       .build();
 
-  ASSERT_NO_THROW(boost::get<StatusType>(response.get()));
+  ASSERT_NO_THROW(boost::get<const StatusType &>(response.get()));
 
   auto proto_status = response.getTransport();
   ASSERT_EQ(proto_status.tx_status(), expected_status);
