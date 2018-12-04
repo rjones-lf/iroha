c5e69267de5cb6a5d742872244a1a2d7606b27e4
Akvinikym <anarant12@gmail.com>
2018-12-04 15:59:18 +0300

    Review issues
    Signed-off-by: Akvinikym <anarant12@gmail.com>

--- shared_model/validators/protobuf/proto_block_validator.hpp
+++ shared_model/validators/protobuf/proto_block_validator.hpp
@@ -6,37 +6,20 @@
 #ifndef IROHA_PROTO_BLOCK_VALIDATOR_HPP
 #define IROHA_PROTO_BLOCK_VALIDATOR_HPP
 
-#include "validators/abstract_validator.hpp"
-
 #include "block.pb.h"
+#include "validators/abstract_validator.hpp"
 
 namespace shared_model {
   namespace validation {
     class ProtoBlockValidator
         : public AbstractValidator<iroha::protocol::Block> {
      private:
-      Answer validateProtoBlock(const iroha::protocol::Block &block) const {
-        Answer answer;
-        std::string tx_reason_name = "Protobuf Block";
-        ReasonsGroupType reason{tx_reason_name, GroupedReasons()};
-
-        // make sure version one_of field of the Block is set
-        if (block.block_version_case()
-            == iroha::protocol::Block::BLOCK_VERSION_NOT_SET) {
-          reason.second.emplace_back("Block version is not set");
-          answer.addReason(std::move(reason));
-          return answer;
-        }
-
-        return answer;
-      }
+      Answer validateProtoBlock(const iroha::protocol::Block &block) const;
 
      public:
-      Answer validate(const iroha::protocol::Block &block) const override {
-        return validateProtoBlock(block);
-      }
+      Answer validate(const iroha::protocol::Block &block) const override;
     };
-  };  // namespace validation
+  }  // namespace validation
 }  // namespace shared_model
 
 #endif  // IROHA_PROTO_BLOCK_VALIDATOR_HPP
