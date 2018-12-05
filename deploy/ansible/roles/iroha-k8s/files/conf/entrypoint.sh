861372a68668b7f56947a04b61fccc6f3b906380
bagf <rory@eishgaming.co.za>
2018-12-05 16:12:57 +0200

    Omit empty blockstore check in container entrypoint script
    Signed-off-by: bagf <rory@eishgaming.co.za>

--- deploy/ansible/roles/iroha-k8s/files/conf/entrypoint.sh
+++ deploy/ansible/roles/iroha-k8s/files/conf/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 # Wait for Postgres container
 sleep 10
-if [ -z "$(ls -A /opt/block_store)" ]; then
-	irohad --genesis_block genesis.block --config config.sample --keypair_name node
-else
-	irohad --config config.sample --keypair_name node
-fi
+irohad --genesis_block genesis.block --config config.sample --keypair_name node
+
