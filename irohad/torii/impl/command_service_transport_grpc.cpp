bacdff23d5efc8c4aa39ae5cee1069674564e87b
Igor Egorov <igor@soramitsu.co.jp>
2019-01-18 19:31:24 +0300

    Apply clang-format
    Signed-off-by: Igor Egorov <igor@soramitsu.co.jp>

--- irohad/torii/impl/command_service_transport_grpc.cpp
+++ irohad/torii/impl/command_service_transport_grpc.cpp
@@ -23,237 +23,235 @@
 
 namespace torii {
 
-    CommandServiceTransportGrpc::CommandServiceTransportGrpc(
-        std::shared_ptr<CommandService> command_service,
-        std::shared_ptr<iroha::torii::StatusBus> status_bus,
-        std::shared_ptr<shared_model::interface::TxStatusFactory>
-            status_factory,
-        std::shared_ptr<TransportFactoryType> transaction_factory,
-        std::shared_ptr<shared_model::interface::TransactionBatchParser>
-            batch_parser,
-        std::shared_ptr<shared_model::interface::TransactionBatchFactory>
-            transaction_batch_factory,
-        rxcpp::observable<ConsensusGateEvent> consensus_gate_objects,
-        int maximum_rounds_without_update,
-        logger::Logger log)
-        : command_service_(std::move(command_service)),
-          status_bus_(std::move(status_bus)),
-          status_factory_(std::move(status_factory)),
-          transaction_factory_(std::move(transaction_factory)),
-          batch_parser_(std::move(batch_parser)),
-          batch_factory_(std::move(transaction_batch_factory)),
-          log_(std::move(log)),
-          consensus_gate_objects_(std::move(consensus_gate_objects)),
-          maximum_rounds_without_update_(maximum_rounds_without_update) {}
+  CommandServiceTransportGrpc::CommandServiceTransportGrpc(
+      std::shared_ptr<CommandService> command_service,
+      std::shared_ptr<iroha::torii::StatusBus> status_bus,
+      std::shared_ptr<shared_model::interface::TxStatusFactory> status_factory,
+      std::shared_ptr<TransportFactoryType> transaction_factory,
+      std::shared_ptr<shared_model::interface::TransactionBatchParser>
+          batch_parser,
+      std::shared_ptr<shared_model::interface::TransactionBatchFactory>
+          transaction_batch_factory,
+      rxcpp::observable<ConsensusGateEvent> consensus_gate_objects,
+      int maximum_rounds_without_update,
+      logger::Logger log)
+      : command_service_(std::move(command_service)),
+        status_bus_(std::move(status_bus)),
+        status_factory_(std::move(status_factory)),
+        transaction_factory_(std::move(transaction_factory)),
+        batch_parser_(std::move(batch_parser)),
+        batch_factory_(std::move(transaction_batch_factory)),
+        log_(std::move(log)),
+        consensus_gate_objects_(std::move(consensus_gate_objects)),
+        maximum_rounds_without_update_(maximum_rounds_without_update) {}
 
-    grpc::Status CommandServiceTransportGrpc::Torii(
-        grpc::ServerContext *context,
-        const iroha::protocol::Transaction *request,
-        google::protobuf::Empty *response) {
-      iroha::protocol::TxList single_tx_list;
-      *single_tx_list.add_transactions() = *request;
-      return ListTorii(context, &single_tx_list, response);
-    }
+  grpc::Status CommandServiceTransportGrpc::Torii(
+      grpc::ServerContext *context,
+      const iroha::protocol::Transaction *request,
+      google::protobuf::Empty *response) {
+    iroha::protocol::TxList single_tx_list;
+    *single_tx_list.add_transactions() = *request;
+    return ListTorii(context, &single_tx_list, response);
+  }
 
-    namespace {
-      /**
-       * Form an error message, which is to be shared between all transactions,
-       * if there are several of them, or individual message, if there's only
-       * one
-       * @param tx_hashes is non empty hash list to form error message from
-       * @param error of those tx(s)
-       * @return message
-       */
-      std::string formErrorMessage(
-          const std::vector<shared_model::crypto::Hash> &tx_hashes,
-          const std::string &error) {
-        if (tx_hashes.size() == 1) {
-          return (boost::format("Stateless invalid tx, error: %s, hash: %s")
-                  % error % tx_hashes[0].hex())
-              .str();
-        }
-
-        std::string folded_hashes =
-            std::accumulate(std::next(tx_hashes.begin()),
-                            tx_hashes.end(),
-                            tx_hashes[0].hex(),
-                            [](auto &&acc, const auto &h) -> std::string {
-                              return acc + ", " + h.hex();
-                            });
-
-        return (boost::format(
-                    "Stateless invalid tx in transaction sequence, error: %s\n"
-                    "Hash list: [%s]")
-                % error % folded_hashes)
+  namespace {
+    /**
+     * Form an error message, which is to be shared between all transactions,
+     * if there are several of them, or individual message, if there's only
+     * one
+     * @param tx_hashes is non empty hash list to form error message from
+     * @param error of those tx(s)
+     * @return message
+     */
+    std::string formErrorMessage(
+        const std::vector<shared_model::crypto::Hash> &tx_hashes,
+        const std::string &error) {
+      if (tx_hashes.size() == 1) {
+        return (boost::format("Stateless invalid tx, error: %s, hash: %s")
+                % error % tx_hashes[0].hex())
             .str();
       }
-    }  // namespace
 
-    shared_model::interface::types::SharedTxsCollectionType
-    CommandServiceTransportGrpc::deserializeTransactions(
-        const iroha::protocol::TxList *request) {
-      shared_model::interface::types::SharedTxsCollectionType tx_collection;
-      for (const auto &tx : request->transactions()) {
-        transaction_factory_->build(tx).match(
-            [&tx_collection](
-                iroha::expected::Value<
-                    std::unique_ptr<shared_model::interface::Transaction>> &v) {
-              tx_collection.emplace_back(std::move(v).value);
-            },
-            [this](iroha::expected::Error<TransportFactoryType::Error> &error) {
-              status_bus_->publish(status_factory_->makeStatelessFail(
-                  error.error.hash,
-                  shared_model::interface::TxStatusFactory::TransactionError{
-                      error.error.error, 0, 0}));
-            });
-      }
-      return tx_collection;
+      std::string folded_hashes =
+          std::accumulate(std::next(tx_hashes.begin()),
+                          tx_hashes.end(),
+                          tx_hashes[0].hex(),
+                          [](auto &&acc, const auto &h) -> std::string {
+                            return acc + ", " + h.hex();
+                          });
+
+      return (boost::format(
+                  "Stateless invalid tx in transaction sequence, error: %s\n"
+                  "Hash list: [%s]")
+              % error % folded_hashes)
+          .str();
+    }
+  }  // namespace
+
+  shared_model::interface::types::SharedTxsCollectionType
+  CommandServiceTransportGrpc::deserializeTransactions(
+      const iroha::protocol::TxList *request) {
+    shared_model::interface::types::SharedTxsCollectionType tx_collection;
+    for (const auto &tx : request->transactions()) {
+      transaction_factory_->build(tx).match(
+          [&tx_collection](
+              iroha::expected::Value<
+                  std::unique_ptr<shared_model::interface::Transaction>> &v) {
+            tx_collection.emplace_back(std::move(v).value);
+          },
+          [this](iroha::expected::Error<TransportFactoryType::Error> &error) {
+            status_bus_->publish(status_factory_->makeStatelessFail(
+                error.error.hash,
+                shared_model::interface::TxStatusFactory::TransactionError{
+                    error.error.error, 0, 0}));
+          });
+    }
+    return tx_collection;
+  }
+
+  grpc::Status CommandServiceTransportGrpc::ListTorii(
+      grpc::ServerContext *context,
+      const iroha::protocol::TxList *request,
+      google::protobuf::Empty *response) {
+    auto transactions = deserializeTransactions(request);
+
+    auto batches = batch_parser_->parseBatches(transactions);
+
+    for (auto &batch : batches) {
+      batch_factory_->createTransactionBatch(batch).match(
+          [&](iroha::expected::Value<std::unique_ptr<
+                  shared_model::interface::TransactionBatch>> &value) {
+            this->command_service_->handleTransactionBatch(
+                std::move(value).value);
+          },
+          [&](iroha::expected::Error<std::string> &error) {
+            std::vector<shared_model::crypto::Hash> hashes;
+
+            std::transform(batch.begin(),
+                           batch.end(),
+                           std::back_inserter(hashes),
+                           [](const auto &tx) { return tx->hash(); });
+
+            auto error_msg = formErrorMessage(hashes, error.error);
+            // set error response for each transaction in a batch candidate
+            std::for_each(
+                hashes.begin(), hashes.end(), [this, &error_msg](auto &hash) {
+                  status_bus_->publish(status_factory_->makeStatelessFail(
+                      hash,
+                      shared_model::interface::TxStatusFactory::
+                          TransactionError{error_msg, 0, 0}));
+                });
+          });
     }
 
-    grpc::Status CommandServiceTransportGrpc::ListTorii(
-        grpc::ServerContext *context,
-        const iroha::protocol::TxList *request,
-        google::protobuf::Empty *response) {
-      auto transactions = deserializeTransactions(request);
+    return grpc::Status::OK;
+  }
 
-      auto batches = batch_parser_->parseBatches(transactions);
+  grpc::Status CommandServiceTransportGrpc::Status(
+      grpc::ServerContext *context,
+      const iroha::protocol::TxStatusRequest *request,
+      iroha::protocol::ToriiResponse *response) {
+    *response =
+        std::static_pointer_cast<shared_model::proto::TransactionResponse>(
+            command_service_->getStatus(
+                shared_model::crypto::Hash::fromHexString(request->tx_hash())))
+            ->getTransport();
+    return grpc::Status::OK;
+  }
 
-      for (auto &batch : batches) {
-        batch_factory_->createTransactionBatch(batch).match(
-            [&](iroha::expected::Value<std::unique_ptr<
-                    shared_model::interface::TransactionBatch>> &value) {
-              this->command_service_->handleTransactionBatch(
-                  std::move(value).value);
-            },
-            [&](iroha::expected::Error<std::string> &error) {
-              std::vector<shared_model::crypto::Hash> hashes;
-
-              std::transform(batch.begin(),
-                             batch.end(),
-                             std::back_inserter(hashes),
-                             [](const auto &tx) { return tx->hash(); });
-
-              auto error_msg = formErrorMessage(hashes, error.error);
-              // set error response for each transaction in a batch candidate
-              std::for_each(
-                  hashes.begin(), hashes.end(), [this, &error_msg](auto &hash) {
-                    status_bus_->publish(status_factory_->makeStatelessFail(
-                        hash,
-                        shared_model::interface::TxStatusFactory::
-                            TransactionError{error_msg, 0, 0}));
-                  });
-            });
+  namespace {
+    void handleEvents(rxcpp::composite_subscription &subscription,
+                      rxcpp::schedulers::run_loop &run_loop) {
+      while (subscription.is_subscribed() or not run_loop.empty()) {
+        run_loop.dispatch();
       }
-
-      return grpc::Status::OK;
     }
+  }  // namespace
 
-    grpc::Status CommandServiceTransportGrpc::Status(
-        grpc::ServerContext *context,
-        const iroha::protocol::TxStatusRequest *request,
-        iroha::protocol::ToriiResponse *response) {
-      *response =
-          std::static_pointer_cast<shared_model::proto::TransactionResponse>(
-              command_service_->getStatus(
-                  shared_model::crypto::Hash::fromHexString(
-                      request->tx_hash())))
+  grpc::Status CommandServiceTransportGrpc::StatusStream(
+      grpc::ServerContext *context,
+      const iroha::protocol::TxStatusRequest *request,
+      grpc::ServerWriter<iroha::protocol::ToriiResponse> *response_writer) {
+    rxcpp::schedulers::run_loop rl;
+
+    auto current_thread =
+        rxcpp::observe_on_one_worker(rxcpp::schedulers::make_run_loop(rl));
+
+    rxcpp::composite_subscription subscription;
+
+    auto hash = shared_model::crypto::Hash::fromHexString(request->tx_hash());
+
+    auto client_id_format = boost::format("Peer: '%s', %s");
+    std::string client_id =
+        (client_id_format % context->peer() % hash.toString()).str();
+
+    auto consensus_gate_observable =
+        consensus_gate_objects_
+            // via scan we do type erasure here
+            .scan(0, [](int seed, const auto &) { return ++seed; })
+            // a dummy start_with lets us don't wait for the consensus event
+            // on further combine_latest
+            .start_with(0);
+
+    boost::optional<iroha::protocol::TxStatus> last_tx_status;
+    auto rounds_counter{0};
+    command_service_
+        ->getStatusStream(hash)
+        // convert to transport objects
+        .map([&](auto response) {
+          log_->info("mapped {}, {}", *response, client_id);
+          return std::static_pointer_cast<
+                     shared_model::proto::TransactionResponse>(response)
               ->getTransport();
-      return grpc::Status::OK;
-    }
+        })
+        .combine_latest(consensus_gate_observable)
+        // complete the observable if client is disconnected or too many
+        // rounds have passed without tx status change
+        .take_while([=, &rounds_counter, &last_tx_status](const auto &tuple) {
+          auto is_cancelled = context->IsCancelled();
+          if (is_cancelled) {
+            log_->debug("client unsubscribed, {}", client_id);
+          }
+          // we increment round counter when the same status arrived again.
+          auto status = std::get<0>(tuple).tx_status();
+          if (last_tx_status and (status == *last_tx_status)) {
+            ++rounds_counter;
+          } else {
+            rounds_counter = 0;
+          }
+          // we stop the stream when round counter is greater than allowed.
+          if (rounds_counter >= maximum_rounds_without_update_) {
+            return false;
+          }
 
-    namespace {
-      void handleEvents(rxcpp::composite_subscription &subscription,
-                        rxcpp::schedulers::run_loop &run_loop) {
-        while (subscription.is_subscribed() or not run_loop.empty()) {
-          run_loop.dispatch();
-        }
-      }
-    }  // namespace
+          return not is_cancelled;
+        })
+        .filter([&last_tx_status](const auto &tuple) {
+          auto status = std::get<0>(tuple).tx_status();
+          // we allow further processing in case of any new status
+          // (including the first one)
+          auto result = not last_tx_status or (*last_tx_status != status);
+          last_tx_status = status;
+          return result;
+        })
+        .subscribe(subscription,
+                   [this, &response_writer, &client_id](const auto &tuple) {
+                     auto response = std::get<0>(tuple);
+                     if (response_writer->Write(response)) {
+                       log_->debug("status written, {}", client_id);
+                     }
+                   },
+                   [&](std::exception_ptr ep) {
+                     log_->error("something bad happened, client_id {}",
+                                 client_id);
+                   },
+                   [&] { log_->debug("stream done, {}", client_id); });
 
-    grpc::Status CommandServiceTransportGrpc::StatusStream(
-        grpc::ServerContext *context,
-        const iroha::protocol::TxStatusRequest *request,
-        grpc::ServerWriter<iroha::protocol::ToriiResponse> *response_writer) {
-      rxcpp::schedulers::run_loop rl;
+    // run loop while subscription is active or there are pending events in
+    // the queue
+    handleEvents(subscription, rl);
 
-      auto current_thread =
-          rxcpp::observe_on_one_worker(rxcpp::schedulers::make_run_loop(rl));
-
-      rxcpp::composite_subscription subscription;
-
-      auto hash = shared_model::crypto::Hash::fromHexString(request->tx_hash());
-
-      auto client_id_format = boost::format("Peer: '%s', %s");
-      std::string client_id =
-          (client_id_format % context->peer() % hash.toString()).str();
-
-      auto consensus_gate_observable =
-          consensus_gate_objects_
-              // via scan we do type erasure here
-              .scan(0, [](int seed, const auto &) { return ++seed; })
-              // a dummy start_with lets us don't wait for the consensus event
-              // on further combine_latest
-              .start_with(0);
-
-      boost::optional<iroha::protocol::TxStatus> last_tx_status;
-      auto rounds_counter{0};
-      command_service_
-          ->getStatusStream(hash)
-          // convert to transport objects
-          .map([&](auto response) {
-            log_->info("mapped {}, {}", *response, client_id);
-            return std::static_pointer_cast<
-                       shared_model::proto::TransactionResponse>(response)
-                ->getTransport();
-          })
-          .combine_latest(consensus_gate_observable)
-          // complete the observable if client is disconnected or too many
-          // rounds have passed without tx status change
-          .take_while([=, &rounds_counter, &last_tx_status](const auto &tuple) {
-            auto is_cancelled = context->IsCancelled();
-            if (is_cancelled) {
-              log_->debug("client unsubscribed, {}", client_id);
-            }
-            // we increment round counter when the same status arrived again.
-            auto status = std::get<0>(tuple).tx_status();
-            if (last_tx_status and (status == *last_tx_status)) {
-              ++rounds_counter;
-            } else {
-              rounds_counter = 0;
-            }
-            // we stop the stream when round counter is greater than allowed.
-            if (rounds_counter >= maximum_rounds_without_update_) {
-              return false;
-            }
-
-            return not is_cancelled;
-          })
-          .filter([&last_tx_status](const auto &tuple) {
-            auto status = std::get<0>(tuple).tx_status();
-            // we allow further processing in case of any new status
-            // (including the first one)
-            auto result = not last_tx_status or (*last_tx_status != status);
-            last_tx_status = status;
-            return result;
-          })
-          .subscribe(subscription,
-                     [this, &response_writer, &client_id](const auto &tuple) {
-                       auto response = std::get<0>(tuple);
-                       if (response_writer->Write(response)) {
-                         log_->debug("status written, {}", client_id);
-                       }
-                     },
-                     [&](std::exception_ptr ep) {
-                       log_->error("something bad happened, client_id {}",
-                                   client_id);
-                     },
-                     [&] { log_->debug("stream done, {}", client_id); });
-
-      // run loop while subscription is active or there are pending events in
-      // the queue
-      handleEvents(subscription, rl);
-
-      log_->debug("status stream done, {}", client_id);
-      return grpc::Status::OK;
-    }
-  }  // namespace torii
+    log_->debug("status stream done, {}", client_id);
+    return grpc::Status::OK;
+  }
+}  // namespace torii
