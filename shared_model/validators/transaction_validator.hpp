2b1aaadfa968151c2368bf4e577bba2daccba83d
Konstantin Munichev <toobwn@gmail.com>
2019-02-18 20:03:39 +0300

    Prevent CommandValidatorVisitor objects from copying
    Signed-off-by: Konstantin Munichev <toobwn@gmail.com>

--- shared_model/validators/transaction_validator.hpp
+++ shared_model/validators/transaction_validator.hpp
@@ -36,11 +36,14 @@ namespace shared_model {
     /**
      * Visitor used by transaction validator to validate each command
      * @tparam FieldValidator - field validator type
+     * @note this class is not a thread safe and never going to
+     * so copy constructor is disabled explicitly
      */
     template <typename FieldValidator>
     class CommandValidatorVisitor
         : public boost::static_visitor<ReasonsGroupType> {
      public:
+      CommandValidatorVisitor(const CommandValidatorVisitor &) = delete;
       CommandValidatorVisitor(
           const FieldValidator &validator = FieldValidator())
           : validator_(validator) {}
@@ -266,7 +269,7 @@ namespace shared_model {
         }
 
         for (const auto &command : tx.commands()) {
-          auto reason = boost::apply_visitor(command_validator_, command.get());
+          auto reason = boost::apply_visitor(CommandValidator(), command.get());
           if (not reason.second.empty()) {
             answer.addReason(std::move(reason));
           }
@@ -277,10 +280,8 @@ namespace shared_model {
 
      public:
       explicit TransactionValidator(
-          const FieldValidator &field_validator = FieldValidator(),
-          const CommandValidator &command_validator = CommandValidator())
-          : field_validator_(field_validator),
-            command_validator_(command_validator) {}
+          const FieldValidator &field_validator = FieldValidator())
+          : field_validator_(field_validator) {}
 
       /**
        * Applies validation to given transaction
@@ -308,7 +309,6 @@ namespace shared_model {
 
      protected:
       FieldValidator field_validator_;
-      CommandValidator command_validator_;
     };
 
   }  // namespace validation
