f40f2ebeae3bddca24d0dca929490cff3f117b30
Andrei Lebedev <lebdron@gmail.com>
2019-03-05 19:52:50 +0300

    Fix round number in SynchronizationEvent
    Signed-off-by: Andrei Lebedev <lebdron@gmail.com>

--- irohad/synchronizer/impl/synchronizer_impl.cpp
+++ irohad/synchronizer/impl/synchronizer_impl.cpp
@@ -102,10 +102,13 @@ namespace iroha {
             auto ledger_state = mutable_factory_->commit(std::move(storage));
 
             if (ledger_state) {
-              return SynchronizationEvent{chain,
-                                          SynchronizationOutcomeType::kCommit,
-                                          msg.round,
-                                          std::move(*ledger_state)};
+              return SynchronizationEvent{
+                  chain,
+                  SynchronizationOutcomeType::kCommit,
+                  blocks.back()->height() > expected_height
+                      ? consensus::Round{blocks.back()->height(), 0}
+                      : msg.round,
+                  std::move(*ledger_state)};
             } else {
               return boost::none;
             }
